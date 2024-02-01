44ff9a213f0e6b38ea870edf0b9a6aad96c783da
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-02-01 08:21:56 -0500

    Update ProcessPortalCancerMetadata.R

--- ProcessPortalCancerMetadata.R
+++ ProcessPortalCancerMetadata.R
@@ -13,6 +13,23 @@
 #	- files.2024-01-30.csv                                                                                            #
 # Obs. A script was created to transform json to csv at :                                                           #
 #      /home/felipe/portal_gdc_cancer_gov/covnert_simply_json.sh                                                    #  
+#     clinical.tsv file was modified to include data_type field from files.2024-01-30.csv                           #
 #####################################################################################################################
+# Set up iput files                                                                                                 #
+# tsv files  are saved as txt and null character '-- replace by -                                                   #
+clinical_file="/home/felipe/portal_gdc_cancer_gov/clinical.txt"                                                     #
+exposure_file="/home/felipe/portal_gdc_cancer_gov/exposure.txt"                                                     #
+famhisto_file="/home/felipe/portal_gdc_cancer_gov/family_history.txt"                                               #
+followup_file="/home/felipe/portal_gdc_cancer_gov/follow_up.txt"                                                    #
+patholog_file="/home/felipe/portal_gdc_cancer_gov/pathology_detail.txt"                                             #
+#####################################################################################################################
+# First, read metadata files                                                                                        #
+clinical_data<-read.table(file = clinical_file, sep = '\t', header = TRUE,fill=TRUE)                                #
+exposure_data<-read.table(file = exposure_file, sep = '\t', header = TRUE,fill=TRUE)                                #
+famhisto_data<-read.table(file = famhisto_file, sep = '\t', header = TRUE,fill=TRUE)                                #
+#####################################################################################################################
+
+
+
 
 

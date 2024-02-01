a0237ece7f97f03fdf190985a7346b66ae8cd273
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-02-01 07:19:42 -0500

    Update ProcessPortalCancerMetadata.R

--- ProcessPortalCancerMetadata.R
+++ ProcessPortalCancerMetadata.R
@@ -1,1 +1,18 @@
-# 
+#####################################################################################################################
+# A script to compile the table descriptive os the cases from the cancer database (https://portal.gdc.cancer.gov/). #
+# Entries:                                                                                                          #
+# A) tar.gz file with Cases (n=44.451).                                                                             #
+#	/home/felipe/portal_gdc_cancer_gov/                                                                               #
+#	- clinical.tsv                                                                                                    # 
+#	- exposure.tsv                                                                                                    # 
+#	- family_history.tsv                                                                                              #
+#	- follow_up.tsv                                                                                                   #
+#	- pathology_detail.tsv                                                                                            #
+# B) json file decription of case file (n=986.114)                                                                  #
+#	- files.2024-01-30.json                                                                                           #
+#	- files.2024-01-30.csv                                                                                            #
+# Obs. A script was created to transform json to csv at :                                                           #
+#      /home/felipe/portal_gdc_cancer_gov/covnert_simply_json.sh                                                    #  
+#####################################################################################################################
+
+

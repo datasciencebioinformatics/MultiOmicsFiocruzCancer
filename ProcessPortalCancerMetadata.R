f239d68ccf1cb4c4b7290fade54f083cc0a7fde4
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-02-02 14:37:13 -0500

    Update ProcessPortalCancerMetadata.R

--- ProcessPortalCancerMetadata.R
+++ ProcessPortalCancerMetadata.R
@@ -6,15 +6,15 @@ library("dendextend")                                                           
 # A script to compile the table descriptive os the cases from the cancer database (https://portal.gdc.cancer.gov/)  #
 # Entries:                                                                                                          #
 # A) tar.gz file with Cases (n=44.451).                                                                             #
-#	/home/felipe/portal_gdc_cancer_gov/                                                                         #
-#	- clinical.tsv                                                                                              # 
-#	- exposure.tsv                                                                                              # 
-#	- family_history.tsv                                                                                        #
-#	- follow_up.tsv                                                                                             #
-#	- pathology_detail.tsv                                                                                      #
+#	/home/felipe/portal_gdc_cancer_gov/                                                                               #
+#	- clinical.tsv                                                                                                    # 
+#	- exposure.tsv                                                                                                    # 
+#	- family_history.tsv                                                                                              #
+#	- follow_up.tsv                                                                                                   #
+#	- pathology_detail.tsv                                                                                            #
 # B) json file decription of case file (n=986.114)                                                                  #
-#	- files.2024-01-30.json                                                                                     #
-#	- files.2024-01-30.csv                                                                                      #
+#	- files.2024-01-30.json                                                                                           #
+#	- files.2024-01-30.csv                                                                                            #
 # Obs. A script was created to transform json to csv at :                                                           #
 #      /home/felipe/portal_gdc_cancer_gov/covnert_simply_json.sh                                                    #  
 #     clinical.tsv file was modified to include data_type field from files.2024-01-30.csv                           #

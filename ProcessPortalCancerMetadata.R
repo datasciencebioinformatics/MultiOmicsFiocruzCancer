d242b654bae184120044b537c4e82c08d74387c6
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-02-07 15:09:24 -0500

    Update ProcessPortalCancerMetadata.R

--- ProcessPortalCancerMetadata.R
+++ ProcessPortalCancerMetadata.R
@@ -398,10 +398,11 @@ print(chisq.test(stu_data))
 print(chisq.test(stu_data))
 #########################################################################################################################################################################################################
 # To Do :
-# Perform test for all co-vararibles
-# Execute tests other than chi-square
-# Categorical variables with more than two levels
-# Anova
+# Perform test for all co-vararibles :
+# 1- Execute tests other than chi-square    
+# 2- Categorical variables with more than two levels
+# 3- Example 4. Regression with one categorical and one continuous predictors (ANCOVA)
+#    Anova
 #########################################################################################################################################################################################################
 
 

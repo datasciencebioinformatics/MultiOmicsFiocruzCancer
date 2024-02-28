45b70dc1261237583d51153f4b0ba3b75e7e025d
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-02-28 10:11:36 -0500

    Update ProcessDataTableFromFile.R

--- ProcessDataTableFromFile.R
+++ ProcessDataTableFromFile.R
@@ -11,6 +11,8 @@ gdc_sample_sheet_data<-read.table(file = gdc_sample_sheet_file, sep = '\t', head
 # Add collumn sample_id
 gdc_sample_sheet_data$sample_submitter_id<-gdc_sample_sheet_data$Sample.ID
 #####################################################################################################################
+output_dir="/home/felipe/Documentos/LungSquaGDC/output/"                                                           #
+#####################################################################################################################
 #To do:
 # - Merge data info with patient info
 # - Associationg between Normal/Tumor and Covariables.
@@ -29,14 +31,58 @@ sample_file="/home/felipe/Documentos/LungSquaGDC/sample.txt"
 clinical_data<-read.table(file = clinical_file, sep = '\t', header = TRUE,fill=TRUE)    
 sample_data<-read.table(file = sample_file, sep = '\t', header = TRUE,fill=TRUE)                                    #
 
-sample_data$case_id 
-clinical_data$case_id
-
 # Merge data
 merged_sample_clinical_data<-merge(sample_data,clinical_data,by="case_id")
 
 # Merge tables
 merged_data_patient_info<-merge(merged_sample_clinical_data,gdc_sample_sheet_data,by="sample_submitter_id")
+
+##########################################################################################################################################
+# Take the colnames
+covariables<-colnames(merged_data_patient_info)
+
+# remove id, code, date, ID, state, code
+covariables<-covariables[-which(grepl("id", covariables))]
+covariables<-covariables[-which(grepl("ID", covariables))]
+covariables<-covariables[-which(grepl("code", covariables))]
+covariables<-covariables[-which(grepl("state", covariables))]
+covariables<-covariables[-which(grepl("date", covariables))]
+covariables<-covariables[-which(grepl("Name", covariables))]
+
+# Remove co-variables
+covariables<-covariables[-which(covariables %in% c("File.ID","File.Name","Data.Category","Data.Type", "Project.ID" ,"Case.ID", "Sample.ID", "sample_submitter_id", "case_id","submitter_id", "project_id.y", "project_id.x", "case_submitter_id.x", "sample_id", "submitter_id", "project_id.y","sample_type_id","submitter_id.1","case_id.1","case_submitter_id.y","created_datetime.1","state.1","submitter_id.2","created_datetime.2","updated_datetime.2","Sample.Type","biospecimen_anatomic_site","biospecimen_laterality","catalog_reference","composition","current_weight","days_to_sample_procurement","diagnosis_pathologically_confirmed","distance_normal_to_tumor","treatment_id","submitter_id","created_datetime","submitter_id","diagnosis_id","pathology_report_uuid","treatment_id","submitter_id"))]
+
+merged_data_patient_info$Diagnosis<-NA
+merged_data_patient_info[factor(merged_data_patient_info$Sample.Type)=="Primary Tumor","Diagnosis"]<-1
+merged_data_patient_info[factor(merged_data_patient_info$Sample.Type)=="Solid Tissue Normal","Diagnosis"]<-0
+
+# Save results
+df_results<-data.frame(Estimate=c(), Std.Error=c(), z.value=c(), pvalue=c())
+
+# For each covariables
+for (covariable in covariables)
+{
+	print(covariable)
+
+	# Try glm function
+	error <- try(glm.full<-glm(formula=formula(paste(" Diagnosis ~ ",covariable,sep="")),family=binomial(link='logit'), data=merged_data_patient_info, na.action=na.omit),TRUE)
+
+	# try-error
+	if (!class(error)[1] == "try-error")
+	{					
+		# Produce glm model			
+		# Take summary
+		df_summary<-summary(glm.full)$coefficients
+	
+		# Rename collumns
+		colnames(df_summary)<-c("Estimate","Std.Error","z.value","pvalue")
+	
+		# Concatenate results
+		df_results<-rbind(df_results,df_summary)
+	}
+}
+## To write a file in Mac Roman for simple use in Mac Excel 2004/8
+write.csv(df_results, file = paste(output_dir,"Association_covariables_Diagnosis.csv",sep="/"))
 
 #####################################################################################################################
 # Set count

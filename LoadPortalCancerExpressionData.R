bd1dda0ab96241d00680e0a6a02c9678bfca5967
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-03-13 12:40:30 -0500

    Update LoadPortalCancerExpressionData.R

--- LoadPortalCancerExpressionData.R
+++ LoadPortalCancerExpressionData.R
@@ -1,1 +1,4 @@
-
+# A script to load cancer data base in R
+unstranded_file  <- /home/felipe/Documentos/LungPortal/samples/unstranded.rna_seq.augmented_star_gene_counts.tsv
+colnames         <- /home/felipe/Documentos/LungPortal/samples/header.txt
+rownames         <- /home/felipe/Documentos/LungPortal/samples/gene_ids.txt

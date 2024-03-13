41223836ecd34ab686baaa48c3237f66b42ac50b
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-03-13 13:34:29 -0500

    Update LoadPortalCancerExpressionData.R

--- LoadPortalCancerExpressionData.R
+++ LoadPortalCancerExpressionData.R
@@ -1,3 +1,59 @@
+##########################################################################################################################################################################################################
+library(readr)
+library("xlsx")
+library(ggplot2)
+# A table containing the Transcriptome Profiling of each sample
+# Read the sample tsv file and for each "Transcriptome Profiling" read the file
+ # Reading the contents of TSV file using read_tsv() method
+gdc_sample_sheet_file<-"/home/felipe/Documentos/LungPortal/gdc_sample_sheet.2024-03-08.tsv"
+
+# Read data
+gdc_sample_sheet_data<-read.table(file = gdc_sample_sheet_file, sep = '\t', header = TRUE,fill=TRUE)  
+
+# Add collumn sample_id
+gdc_sample_sheet_data$sample_submitter_id<-gdc_sample_sheet_data$Sample.ID
+#####################################################################################################################
+output_dir="/home/felipe/Documentos/LungPortal/output/"                                                           #
+#####################################################################################################################
+#To do:
+# - Merge data info with patient info
+# - Associationg between Normal/Tumor and Covariables.
+# - Check criterias for selectig data e.g. "Transcriptome Profiling" only. e.g. ".FPKM.txt.gz"
+# - Outputs : a) a table dataInfo + patientInfo.
+# -           b) A table with expression of selected patients.
+# - To plan : 1) association covariable~primary_diagnosis
+# -           2) association covariable~Normal/Tumor
+# - To study : Literature of association co-variables ~ cancer
+#####################################################################################################################
+# Set path to files
+clinical_file="/home/felipe/Documentos/LungPortal/clinical.txt" 
+sample_file="/home/felipe/Documentos/LungPortal/sample.txt"    
+exposure_file="/home/felipe/Documentos/LungPortal/exposure.txt"                                                  #
+
+# Load data
+clinical_data<-read.table(file = clinical_file, sep = '\t', header = TRUE,fill=TRUE)    
+sample_data<-read.table(file = sample_file, sep = '\t', header = TRUE,fill=TRUE)                                    #
+exposure_data<-read.table(file = exposure_file, sep = '\t', header = TRUE,fill=TRUE)                                #
+
+# Merge data
+merged_sample_clinical_data<-merge(sample_data,clinical_data,by="case_id")
+
+# Merge all
+merged_sample_clinical_data<-merge(merged_sample_clinical_data,exposure_data,by="case_id")
+
+# Merge tables
+merged_data_patient_info<-merge(merged_sample_clinical_data,gdc_sample_sheet_data,by="sample_submitter_id")
+#####################################################################################################################
+# length(unique(merged_data_patient_info$case_id)) # Number of cases
+# length(unique(merged_data_patient_info$sample_id)) # Number of samples
+# sum(unique(merged_data_patient_info[,c("sample_id","Sample.Type")])[,2]=="Primary Tumor") # Number of Primary Tumor
+# sum(unique(merged_data_patient_info[,c("sample_id","Sample.Type")])[,2]=="Solid Tissue Normal") # Number of Solid Tissue Normal
+
+# Filter tumor and normal samples
+primary_tumor<-merged_data_patient_info[merged_data_patient_info[,c("sample_id","Sample.Type")][,2]=="Primary Tumor",]
+solid_tissue<-merged_data_patient_info[merged_data_patient_info[,c("sample_id","Sample.Type")][,2]=="Solid Tissue Normal",]
+merged_data_patient_info<-rbind(primary_tumor,solid_tissue)
+#####################################################################################################################
 # A script to load cancer data base in R
 unstranded_file       <- "/home/felipe/Documentos/LungPortal/samples/unstranded.rna_seq.augmented_star_gene_counts.tsv"
 colnames_file         <- "/home/felipe/Documentos/LungPortal/samples/header.txt"

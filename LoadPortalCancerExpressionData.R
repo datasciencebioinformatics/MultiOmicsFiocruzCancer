504066c55cca8dde4cc8d2b0fe4c18671e8ca2f1
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-03-13 13:30:46 -0500

    Update LoadPortalCancerExpressionData.R

--- LoadPortalCancerExpressionData.R
+++ LoadPortalCancerExpressionData.R
@@ -1,4 +1,16 @@
 # A script to load cancer data base in R
-unstranded_file  <- /home/felipe/Documentos/LungPortal/samples/unstranded.rna_seq.augmented_star_gene_counts.tsv
-colnames         <- /home/felipe/Documentos/LungPortal/samples/header.txt
-rownames         <- /home/felipe/Documentos/LungPortal/samples/gene_ids.txt
+unstranded_file       <- "/home/felipe/Documentos/LungPortal/samples/unstranded.rna_seq.augmented_star_gene_counts.tsv"
+colnames_file         <- "/home/felipe/Documentos/LungPortal/samples/header.txt"
+rownames_file         <- "/home/felipe/Documentos/LungPortal/samples/gene_ids.txt"
+
+# Load data
+unstranded_data<-read.table(file = unstranded_file, sep = '\t', header = FALSE,fill=TRUE)    
+colnames_data<-read.table(file = colnames_file, sep = '\t', header = FALSE,fill=TRUE)                                    #
+rownames_data<-read.table(file = rownames_file, sep = '\t', header = FALSE,fill=TRUE)      
+
+# Set colnames and rownames
+rownames(unstranded_data)<-rownames_data[,1]
+colnames(unstranded_data)<-colnames_data[,1]
+
+# To check counts and sample_ids
+

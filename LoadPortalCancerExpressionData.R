06cddf66701e5ee699d0a521e72a90a5850d774d
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-03-14 07:43:37 -0500

    Update LoadPortalCancerExpressionData.R

--- LoadPortalCancerExpressionData.R
+++ LoadPortalCancerExpressionData.R
@@ -1,55 +1,14 @@
-##########################################################################################################################################################################################################
 library(readr)
 library("xlsx")
 library(ggplot2)
-# A table containing the Transcriptome Profiling of each sample
-# Read the sample tsv file and for each "Transcriptome Profiling" read the file
- # Reading the contents of TSV file using read_tsv() method
-gdc_sample_sheet_file<-"/home/felipe/Documentos/LungPortal/gdc_sample_sheet.2024-03-08.tsv"
-
-# Read data
-gdc_sample_sheet_data<-read.table(file = gdc_sample_sheet_file, sep = '\t', header = TRUE,fill=TRUE)  
-
-# Add collumn sample_id
-gdc_sample_sheet_data$sample_submitter_id<-gdc_sample_sheet_data$Sample.ID
+##########################################################################################################################################################################################################
+output_dir="/home/felipe/Documentos/LungPortal/output/"                                                             #
 #####################################################################################################################
-output_dir="/home/felipe/Documentos/LungPortal/output/"                                                           #
-#####################################################################################################################
-#To do:
-# - Merge data info with patient info
-# - Associationg between Normal/Tumor and Covariables.
-# - Check criterias for selectig data e.g. "Transcriptome Profiling" only. e.g. ".FPKM.txt.gz"
-# - Outputs : a) a table dataInfo + patientInfo.
-# -           b) A table with expression of selected patients.
-# - To plan : 1) association covariable~primary_diagnosis
-# -           2) association covariable~Normal/Tumor
-# - To study : Literature of association co-variables ~ cancer
-#####################################################################################################################
-# Set path to files
-clinical_file="/home/felipe/Documentos/LungPortal/clinical.txt" 
-sample_file="/home/felipe/Documentos/LungPortal/sample.txt"    
-exposure_file="/home/felipe/Documentos/LungPortal/exposure.txt"                                                  #
+# Reading the contents of TSV file using read_tsv() method
+merged_data_patient_info_file<- "/home/felipe/Documentos/LungPortal/samples/merged_data_patient_info.tsv"
 
-# Load data
-clinical_data<-read.table(file = clinical_file, sep = '\t', header = TRUE,fill=TRUE)    
-sample_data<-read.table(file = sample_file, sep = '\t', header = TRUE,fill=TRUE)                                    #
-exposure_data<-read.table(file = exposure_file, sep = '\t', header = TRUE,fill=TRUE)                                #
-
-# Merge data
-merged_sample_clinical_data<-merge(sample_data,clinical_data,by="case_id")
-
-# Merge all
-merged_sample_clinical_data<-merge(merged_sample_clinical_data,exposure_data,by="case_id")
-
-# Merge tables
-merged_data_patient_info<-merge(merged_sample_clinical_data,gdc_sample_sheet_data,by="sample_submitter_id")
-#####################################################################################################################
-# Set file name variable 
-# From the File.ID, only the ID is kept in the variable sample_id
-merged_data_patient_info$sample_id<-gsub(".rna_seq.augmented_star_gene_counts.tsv", "", merged_data_patient_info$File.Name)
-
-# Filter the table to contain only samples in the dataset
-merged_data_patient_info<-merged_data_patient_info[merged_data_patient_info$sample_id %in% colnames(unstranded_data),]
+# Load metadata table
+merged_data_patient_info     <-read.table(file = merged_data_patient_info, sep = '\t', header = TRUE,fill=TRUE)   
 #####################################################################################################################
 # A script to load cancer data base in R
 unstranded_file       <- "/home/felipe/Documentos/LungPortal/samples/unstranded.rna_seq.augmented_star_gene_counts.tsv"
@@ -58,7 +17,7 @@ rownames_file         <- "/home/felipe/Documentos/LungPortal/samples/gene_ids.tx
 
 # Load data
 unstranded_data<-read.table(file = unstranded_file, sep = '\t', header = FALSE,fill=TRUE)    
-colnames_data<-read.table(file = colnames_file, sep = '\t', header = FALSE,fill=TRUE)                                    #
+colnames_data<-read.table(file = colnames_file, sep = '\t', header = FALSE,fill=TRUE)                                    
 rownames_data<-read.table(file = rownames_file, sep = '\t', header = FALSE,fill=TRUE)      
 
 # Set colnames and rownames
@@ -81,6 +40,9 @@ merged_data_patient_info$stages<-gsub("Stage IIIB", "Stage III", merged_data_pat
 merged_data_patient_info$stages<-gsub("Stage IVA", "Stage IV", merged_data_patient_info$stages)
 merged_data_patient_info$stages<-gsub("Stage IVB", "Stage IV", merged_data_patient_info$stages)
 
+# Filter the table to contain only samples in the dataset
+merged_data_patient_info<-merged_data_patient_info[merged_data_patient_info$sample_id %in% colnames(unstranded_data),]
+
 # Save table with samples and metatada
 # There are 238 cases, 570 samples, 478 tumor, 92 normal
 
@@ -96,3 +58,9 @@ write_tsv(merged_data_patient_info, "/home/felipe/Documentos/LungPortal/samples/
 
 # Creat DEseq element from unstranded_data and merged_data_patient_info
 dds <- DESeqDataSetFromMatrix(countData = unstranded_data, colData=merged_data_patient_info, design = ~sample_id + age_at_index + gender + stages)
+
+# Run DESeq2
+dds <- DESeq(dds)
+
+# Obtain differential expression numbers
+res <- results(dds, alpha=0.05, lfcThreshold=log2(2))

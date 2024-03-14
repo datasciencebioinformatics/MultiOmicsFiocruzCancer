5e8207b280f6fa89ff317c71deeff77b812a20cd
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-03-14 07:42:25 -0500

    Update ProcessPortalDataFromFile.R

--- ProcessPortalDataFromFile.R
+++ ProcessPortalDataFromFile.R
@@ -2,9 +2,8 @@
 library(readr)
 library("xlsx")
 library(ggplot2)
-# A table containing the Transcriptome Profiling of each sample
-# Read the sample tsv file and for each "Transcriptome Profiling" read the file
- # Reading the contents of TSV file using read_tsv() method
+##########################################################################################################################################################################################################
+# Reading the contents of TSV file using read_tsv() method
 gdc_sample_sheet_file<-"/home/felipe/Documentos/LungPortal/gdc_sample_sheet.2024-03-08.tsv"
 
 # Read data
@@ -13,26 +12,16 @@ gdc_sample_sheet_data<-read.table(file = gdc_sample_sheet_file, sep = '\t', head
 # Add collumn sample_id
 gdc_sample_sheet_data$sample_submitter_id<-gdc_sample_sheet_data$Sample.ID
 #####################################################################################################################
-output_dir="/home/felipe/Documentos/LungPortal/output/"                                                           #
+output_dir="/home/felipe/Documentos/LungPortal/output/"                                                             #
 #####################################################################################################################
-#To do:
-# - Merge data info with patient info
-# - Associationg between Normal/Tumor and Covariables.
-# - Check criterias for selectig data e.g. "Transcriptome Profiling" only. e.g. ".FPKM.txt.gz"
-# - Outputs : a) a table dataInfo + patientInfo.
-# -           b) A table with expression of selected patients.
-# - To plan : 1) association covariable~primary_diagnosis
-# -           2) association covariable~Normal/Tumor
-# - To study : Literature of association co-variables ~ cancer
-#####################################################################################################################
-# Set path to files
+# Set path to files                                                                                                 
 clinical_file="/home/felipe/Documentos/LungPortal/clinical.txt" 
 sample_file="/home/felipe/Documentos/LungPortal/sample.txt"    
-exposure_file="/home/felipe/Documentos/LungPortal/exposure.txt"                                                  #
+exposure_file="/home/felipe/Documentos/LungPortal/exposure.txt"                                                  
 
 # Load data
 clinical_data<-read.table(file = clinical_file, sep = '\t', header = TRUE,fill=TRUE)    
-sample_data<-read.table(file = sample_file, sep = '\t', header = TRUE,fill=TRUE)                                    #
+sample_data<-read.table(file = sample_file, sep = '\t', header = TRUE,fill=TRUE)                                    
 exposure_data<-read.table(file = exposure_file, sep = '\t', header = TRUE,fill=TRUE)                                #
 
 # Merge data
@@ -44,6 +33,14 @@ merged_sample_clinical_data<-merge(merged_sample_clinical_data,exposure_data,by=
 # Merge tables
 merged_data_patient_info<-merge(merged_sample_clinical_data,gdc_sample_sheet_data,by="sample_submitter_id")
 #####################################################################################################################
+# Set file name variable 
+# From the File.ID, only the ID is kept in the variable sample_id
+merged_data_patient_info$sample_id<-gsub(".rna_seq.augmented_star_gene_counts.tsv", "", merged_data_patient_info$File.Name)
+
+# Organize how to send to Carles
+write_tsv(merged_data_patient_info, "/home/felipe/Documentos/LungPortal/samples/merged_data_patient_info.tsv")
+#####################################################################################################################
+
 # length(unique(merged_data_patient_info$case_id)) # Number of cases
 # length(unique(merged_data_patient_info$sample_id)) # Number of samples
 # sum(unique(merged_data_patient_info[,c("sample_id","Sample.Type")])[,2]=="Primary Tumor") # Number of Primary Tumor

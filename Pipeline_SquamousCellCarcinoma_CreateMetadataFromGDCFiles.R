533d50bda88cd1b288c96d94030780c7a66d07b6
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-03-25 06:43:41 -0500

    Update Pipeline_SquamousCellCarcinoma_CreateMetadataFromGDCFiles.R

--- Pipeline_SquamousCellCarcinoma_CreateMetadataFromGDCFiles.R
+++ Pipeline_SquamousCellCarcinoma_CreateMetadataFromGDCFiles.R
@@ -1,3 +1,10 @@
+# A R script to create metadata from gdc files.
+# Inputs:
+# gdc_sample_sheet.2024-03-08.tsv
+# /home/felipe/Documentos/LungPortal/clinical.txt
+# /home/felipe/Documentos/LungPortal/sample.txt
+# /home/felipe/Documentos/LungPortal/exposure.txt
+# Output : merged_data_patient_info.tsv
 ##########################################################################################################################################################################################################
 library(readr)
 library("xlsx")

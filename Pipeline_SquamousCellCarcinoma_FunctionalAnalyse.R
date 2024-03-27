47cc7caa3c746b2f8baa60907b3188727deb3a7b
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-03-27 06:23:08 -0500

    Update Pipeline_SquamousCellCarcinoma_FunctionalAnalyse.R

--- Pipeline_SquamousCellCarcinoma_FunctionalAnalyse.R
+++ Pipeline_SquamousCellCarcinoma_FunctionalAnalyse.R
@@ -1,1 +1,13 @@
-
+#######################################################################################################################################
+# Path to files of selected_genes                                                                                                     # 
+selected_genes_Stage_I_file       <-"/home/felipe/Documentos/LungPortal/output/selected_genes_Stage_I.tsv"                            #
+selected_genes_Stage_II_file      <-"/home/felipe/Documentos/LungPortal/output/selected_genes_Stage_II.tsv"                           #
+selected_genes_Stage_III_file     <-"/home/felipe/Documentos/LungPortal/output/selected_genes_Stage_III.tsv"                          #
+selected_genes_Stage_I_III_file   <-"/home/felipe/Documentos/LungPortal/output/selected_genes_Stage_I_III.tsv"                        #
+#######################################################################################################################################
+# Load data                                                                                                                           #
+selected_genes_Stage_I_data       <-read.table(file = selected_genes_Stage_I_file, sep = '\t', header = TRUE,fill=TRUE)               #
+selected_genes_Stage_II_data      <-read.table(file = selected_genes_Stage_II_file, sep = '\t', header = TRUE,fill=TRUE)              #
+selected_genes_Stage_III_data     <-read.table(file = selected_genes_Stage_III_file, sep = '\t', header = TRUE,fill=TRUE)             #
+#######################################################################################################################################
+list()

81c1934be273b0fde972468166da2e88f5354637
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-03-27 06:52:42 -0500

    Update Pipeline_SquamousCellCarcinoma_FunctionalAnalyse.R

--- Pipeline_SquamousCellCarcinoma_FunctionalAnalyse.R
+++ Pipeline_SquamousCellCarcinoma_FunctionalAnalyse.R
@@ -9,5 +9,14 @@ selected_genes_Stage_I_III_file   <-"/home/felipe/Documentos/LungPortal/output/s
 selected_genes_Stage_I_data       <-read.table(file = selected_genes_Stage_I_file, sep = '\t', header = TRUE,fill=TRUE)               #
 selected_genes_Stage_II_data      <-read.table(file = selected_genes_Stage_II_file, sep = '\t', header = TRUE,fill=TRUE)              #
 selected_genes_Stage_III_data     <-read.table(file = selected_genes_Stage_III_file, sep = '\t', header = TRUE,fill=TRUE)             #
+selected_genes_Stage_I_III_data   <-read.table(file = selected_genes_Stage_I_III_file, sep = '\t', header = TRUE,fill=TRUE)           #
 #######################################################################################################################################
-list()
+library(ggVennDiagram)                                                                                                                #
+stages_I_II_III<-ggVennDiagram(list(Stage_I    =selected_genes_Stage_I_data$Gene,Stage_III  =selected_genes_Stage_III_data$Gene,Stage_II   =selected_genes_Stage_II_data$Gene), label_alpha = 0) + scale_fill_viridis() + theme_bw() + ggtitle("Stages I, II and III")
+stages_I_vs_III<-ggVennDiagram(list(Stages_I    =selected_genes_Stage_I_data$Gene, Stages_III    =selected_genes_Stage_III_data$Gene, Stages_II_vs_III    =selected_genes_Stage_I_III_data$Gene), label_alpha = 0) + scale_fill_viridis() + theme_bw() + ggtitle("Stages I, III and Stage I vs III")
+#######################################################################################################################################
+
+
+
+
+

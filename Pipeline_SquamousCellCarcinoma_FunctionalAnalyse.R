1985349cfde032886c6753c3c4e8b9a9100e0885
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-03-28 15:08:22 -0500

    Update Pipeline_SquamousCellCarcinoma_FunctionalAnalyse.R

--- Pipeline_SquamousCellCarcinoma_FunctionalAnalyse.R
+++ Pipeline_SquamousCellCarcinoma_FunctionalAnalyse.R
@@ -14,6 +14,9 @@ stage_I_sim_matrix <- calculateSimMatrix(selected_genes_Stage_I_data$Gene,orgdb=
 stage_II_sim_matrix <- calculateSimMatrix(selected_genes_Stage_II_data$Gene,orgdb="org.Hs.eg.db",ont="BP", method="Rel")
 stage_III_sim_matrix <- calculateSimMatrix(selected_genes_Stage_III_data$Gene,orgdb="org.Hs.eg.db",ont="BP", method="Rel")
 #######################################################################################################################################
+library("topGO")
+sampleGOdata <- new("topGOdata",description = "Simple session", ontology = "BP",allGenes = list(selected_genes_Stage_I_data$Gene, selected_genes_Stage_II_data$Gene, selected_genes_Stage_III_data$Gene), geneSel = list(selected_genes_Stage_I_data$Gene, selected_genes_Stage_II_data$Gene, selected_genes_Stage_III_data$Gene), nodeSize = 10,annot = annFUN.db, affyLib = affyLib)
+#######################################################################################################################################
 # https://bioconductor.org/packages/release/bioc/vignettes/rrvgo/inst/doc/rrvgo.html
 scores <- setNames(-log10(stage_I_sim_matrix$qvalue), stage_I_sim_matrix$ID)
 reducedTerms <- reduceSimMatrix(stage_I_sim_matrix,

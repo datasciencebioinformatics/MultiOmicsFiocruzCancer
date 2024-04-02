b2358f983b2f52283b34bbf0d364623bb7683c01
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-02 12:11:20 -0500

    Update Pipeline_SquamousCellCarcinoma_LoadExpressionData.R

--- Pipeline_SquamousCellCarcinoma_LoadExpressionData.R
+++ Pipeline_SquamousCellCarcinoma_LoadExpressionData.R
@@ -122,7 +122,7 @@ unstranded_data<-unstranded_data[selected_gene_id,]
 merged_data_patient_info$patient_id<-paste("patient_",1:length(merged_data_patient_info$sample_id),sep="")
 
 # Writing mtcars data
-write.table(unstranded_data, file = "/home/felipe/Documentos/LungPortal/samples/patient.metadata.tsv", sep = "\t", row.names = TRUE, col.names = TRUE)
+write.table(colData, file = "/home/felipe/Documentos/LungPortal/samples/colData.tsv", sep = "\t", row.names = TRUE, col.names = TRUE)
 write.table(merged_data_patient_info[,c("patient_id","case_id","sample_id","age_at_index","gender","tumor_normal","stages")], file ="/home/felipe/Documentos/LungPortal/samples/patient.metadata.tsv" , sep = "\t", row.names = TRUE, col.names = TRUE)
-write.table(colData, file = "/home/felipe/Documentos/LungPortal/samples/unstranded_data_id.tsv", sep = "\t", row.names = TRUE, col.names = TRUE)
+write.table(unstranded_data, file = "/home/felipe/Documentos/LungPortal/samples/unstranded_data_id.tsv", sep = "\t", row.names = TRUE, col.names = TRUE)
 #####################################################################################################################

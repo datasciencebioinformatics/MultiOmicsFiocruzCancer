bb68e5e8a4d7f0b85a8abf2c7c4e1b6837e810c7
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-03 10:07:09 -0500

    Update Pipeline_SquamousCellCarcinoma_CreateMetadataPairedSamples.R

--- Pipeline_SquamousCellCarcinoma_CreateMetadataPairedSamples.R
+++ Pipeline_SquamousCellCarcinoma_CreateMetadataPairedSamples.R
@@ -12,8 +12,8 @@ library(ggplot2)
 ##########################################################################################################################################################################################################
 output_dir="/home/felipe/Documentos/LungPortal/output/"                                                                   #
 ###########################################################################################################################
-unstranded_data_file                <- "/home/felipe/Documentos/LungPortal/samples/unstranded.tsv"                        #
-merged_data_patient_info_file       <- "/home/felipe/Documentos/LungPortal/samples/patient.metadata.tsv"                  #
+unstranded_data_file                <- "/home/felipe/Documentos/LungPortal/samples/unstranded_data_filtered.tsv"          #
+merged_data_patient_info_file       <- "/home/felipe/Documentos/LungPortal/samples/patient.metadata.filetered.tsv"        #
 colData_file                        <- "/home/felipe/Documentos/LungPortal/samples/colData.tsv"                           #
 ###########################################################################################################################
 unstranded_data                    <-read.table(file = unstranded_data_file, sep = '\t', header = TRUE,fill=TRUE)         #
@@ -29,14 +29,13 @@ dds_stages_tissue_type <- estimateSizeFactors(dds_stages_tissue_type)
 # Obtain normalized coutns
 norm_counts<-counts(dds_stages_tissue_type, normalized = TRUE)
 ###########################################################################################################################
-
 # Paired samples
 paired_sample_df<-data.frame(normal=c(normal_samples_id),tumor=c(tumor_solid_sample_id),case=case)
 
 # For each case, find the pairs
 for (case in unique(merged_data_patient_info_data$case))
 {
-    # Take the samples of this case (normal and tumor)
+    # All samples for case id = "case"
     case_samples<-merged_data_patient_info_data[merged_data_patient_info_data$case==case,]
 
     # Take the tumor samples
@@ -47,10 +46,10 @@ for (case in unique(merged_data_patient_info_data$case))
     if(length(unique(normal_sampĺes$sample_id))>0 && length(unique(tumor_sampĺes$sample_id))>0)
     {
             # For each tumor sample
-            for (tumor_solid_sample_id in tumor_sampĺes$sample_id)
+            for (tumor_solid_sample_id in tumor_sampĺes$patient_id)
             {
                 # for each normal sample, compile a paired samples
-                for (normal_samples_id in normal_sampĺes$sample_id)
+                for (normal_samples_id in normal_sampĺes$patient_id)
                 {
                     # Contatenate                     
                     paired_sample_df<-rbind(data.frame(normal=c(normal_samples_id),tumor=c(tumor_solid_sample_id),case=case),paired_sample_df)
@@ -58,41 +57,50 @@ for (case in unique(merged_data_patient_info_data$case))
             }                
     }
 }
+###########################################################################################################################
+# Data.frame to store results for control samples
+df_diff_expression<-data.frame(Gene=rownames(unstranded_data))
+
+# set rownames
+rownames(df_diff_expression)<-df_diff_expression$Gene
+
 # For each pair, we subtracted gene expression values of control samples
-for (case in paired_sample_df$case)
+for (case in rownames(paired_sample_df))
 {    
-    # Take samples from the case
-    # Important here is to check patiend_id field
-    # it must e consistent in the data.frames merged_data_patient_info_data and colData 
-    cases_samples<-merged_data_patient_info_data[which(merged_data_patient_info_data$case==case),]
+
+    case_sample  <-paired_sample_df[case,"case"]
+    normal_sample<-paired_sample_df[case,"normal"]
+    tumor_sample <-paired_sample_df[case,"tumor"]    
    
-    # Take tumor and normal
-    # Here I take the id of normal samples and the id of the tumor samples
-    normal_sample<-paired_sample_df[paired_sample_df$case==case,"normal"]
-    tumor_sample <-paired_sample_df[paired_sample_df$case==case,"tumor"]
+    # Normal and tumor samples for this "case" id
+    normal_sample<-merged_data_patient_info_data[which(merged_data_patient_info_data$patient_id==normal_sample),]
+    tumor_sample <-merged_data_patient_info_data[which(merged_data_patient_info_data$patient_id==tumor_sample),]
 
-    # Take tumor and normal
-    # Here I must take the the field patient_id from normal samples
-    cases_samples_normal<-merged_data_patient_info_data[which(merged_data_patient_info_data$sample_id==normal_sample),"patient_id"]
+    # Store results for the case
+    case_results_normal<-data.frame(case=norm_counts[,c(normal_sample$patient_id)])
+    case_results_tumor <-data.frame(case=norm_counts[,c(tumor_sample$patient_id)])
+    diff_expression<-case_results_tumor-case_results_normal
 
-    # Here I must take the the field patient_id from normal samples
-    cases_samples_tumor<-merged_data_patient_info_data[which(merged_data_patient_info_data$sample_id==tumor_sample),"patient_id"]
+    # Rename collumns
+    colnames(diff_expression)<-case_sample
 
-    # For each normal samples
-    for (normal_id in normal_ids)
-    {
-        # Take tumor samples
-        tumor_sample <-paired_sample_df[paired_sample_df$case==case,"tumor"]
-
-        # Take tumor ids
-        tumor_ids <-cases_samples[which(cases_samples$sample_id==tumor_sample),"patient_id"]
-
-        # For each normal samples
-        for (tumor_id in tumor_ids)
-        {
-            print(paste(normal_id,tumor_id) )
-        }        
-    }    
+    # Concatenate 
+    df_diff_expression<-cbind(df_diff_expression,diff_expression)    
 }
+# create dictionary for patient_id and case_id
+df_patient_case_id<-data.frame(case=merged_data_patient_info_data$case,patient_id=merged_data_patient_info_data$patient_id)
+
+# Rename colnames()
+df_patient_case_id
+
+colnames(df_diff_expression) %in% df_patient_case_id$case
+
+
+
+
+
+
+
+
 
 

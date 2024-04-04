63a5a38a4a6f339c62c87e45da170697cbdf2c69
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-04 12:47:22 -0500

    Update Pipeline_SquamousCellCarcinoma_CreateMetadataPairedSamples.R

--- Pipeline_SquamousCellCarcinoma_CreateMetadataPairedSamples.R
+++ Pipeline_SquamousCellCarcinoma_CreateMetadataPairedSamples.R
@@ -23,6 +23,7 @@ rownames(colData)                  <-colData$patient_id                         
 ###########################################################################################################################
 colData<-na.omit(colData)
 unstranded_data<-unstranded_data[,colData$patient_id]
+merged_data_patient_info_data<-merged_data_patient_info_data[which(merged_data_patient_info_data$patient_id %in% colData$patient_id),]
 ###########################################################################################################################
 dds_stages_tissue_type <- DESeqDataSetFromMatrix(countData = unstranded_data, colData=colData_data[colnames(unstranded_data),], design = ~  age_at_index +  gender +tissue_type  )
 ###########################################################################################################################

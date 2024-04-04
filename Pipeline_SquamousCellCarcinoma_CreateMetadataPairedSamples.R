05423cc1a7a3426a5112e8e1fcf6718295e12f7b
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-04 12:48:26 -0500

    Update Pipeline_SquamousCellCarcinoma_CreateMetadataPairedSamples.R

--- Pipeline_SquamousCellCarcinoma_CreateMetadataPairedSamples.R
+++ Pipeline_SquamousCellCarcinoma_CreateMetadataPairedSamples.R
@@ -21,10 +21,6 @@ merged_data_patient_info_data      <-read.table(file = merged_data_patient_info_
 colData_data                       <-read.table(file = colData_file, sep = '\t', header = TRUE,fill=TRUE)                 #
 rownames(colData)                  <-colData$patient_id                                                                   #
 ###########################################################################################################################
-colData<-na.omit(colData)
-unstranded_data<-unstranded_data[,colData$patient_id]
-merged_data_patient_info_data<-merged_data_patient_info_data[which(merged_data_patient_info_data$patient_id %in% colData$patient_id),]
-###########################################################################################################################
 dds_stages_tissue_type <- DESeqDataSetFromMatrix(countData = unstranded_data, colData=colData_data[colnames(unstranded_data),], design = ~  age_at_index +  gender +tissue_type  )
 ###########################################################################################################################
 # Estimate size factor

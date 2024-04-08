4525cb74d39870300c75a10b602706a8243fdf57
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-08 07:18:06 -0500

    Update Pipeline_SquamousCellCarcinoma_CreateMetadataPairedSamples.R

--- Pipeline_SquamousCellCarcinoma_CreateMetadataPairedSamples.R
+++ Pipeline_SquamousCellCarcinoma_CreateMetadataPairedSamples.R
@@ -98,7 +98,7 @@ df_diff_expression<-df_diff_expression[,-1]
 avg_expression<-data.frame(rowMeans(df_diff_expression))
 
 # Take average expression of positive sample
-avg_expression_pos<-data.frame(Gene=rownames(avg_expression)[which(avg_expression>=1.5)],Expression=avg_expression[which(avg_expression>=1.5),])
+avg_expression_pos<-data.frame(Gene=rownames(avg_expression)[which(avg_expression>=1.0)],Expression=avg_expression[which(avg_expression>=1.0),])
 
 # Take average expression of positive sample
 rownames(avg_expression_pos)<-avg_expression_pos$Gene

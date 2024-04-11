507fba9a7894f55cae7b5c118d3c21a50b356685
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-11 12:08:01 -0500

    Update Pipeline_SquamousCellCarcinoma_VeenDiagrams.R

--- Pipeline_SquamousCellCarcinoma_VeenDiagrams.R
+++ Pipeline_SquamousCellCarcinoma_VeenDiagrams.R
@@ -10,11 +10,11 @@ selected_genes_Stage_III_file     <-"/home/felipe/Documentos/LungPortal/output/s
 selected_genes_Stage_I_data       <-read.table(file = selected_genes_Stage_I_file, sep = '\t', header = TRUE,fill=TRUE)               #
 selected_genes_Stage_II_data      <-read.table(file = selected_genes_Stage_II_file, sep = '\t', header = TRUE,fill=TRUE)              #
 selected_genes_Stage_III_data     <-read.table(file = selected_genes_Stage_III_file, sep = '\t', header = TRUE,fill=TRUE)             #
-
-# Set rownames
-rownames(selected_genes_Stage_I_data)<-selected_genes_Stage_I_data$Gene
-rownames(selected_genes_Stage_II_data)<-selected_genes_Stage_II_data$Gene
-rownames(selected_genes_Stage_III_data)<-selected_genes_Stage_III_data$Gene
+                                                                                                                                      #
+# Set rownames                                                                                                                        #
+rownames(selected_genes_Stage_I_data)<-selected_genes_Stage_I_data$Gene                                                               #
+rownames(selected_genes_Stage_II_data)<-selected_genes_Stage_II_data$Gene                                                             #
+rownames(selected_genes_Stage_III_data)<-selected_genes_Stage_III_data$Gene                                                           #
 #######################################################################################################################################
 # Select stages 
 stages_I_II_III<-ggVennDiagram(list(Stage_I    =selected_genes_Stage_I_data$Gene,Stage_II  =selected_genes_Stage_II_data$Gene,Stage_III  =selected_genes_Stage_III_data$Gene), label_alpha = 0) + scale_fill_viridis() + theme_bw() + ggtitle("Stages I, II and III")
@@ -40,24 +40,24 @@ png(filename=paste(output_dir,"Veen_diagrams.png",sep=""), width = 28, height = 
   pca_plots<-grid.arrange( stages_I_II_III,stages_I_II_III_unique,  ncol = 2)
 dev.off()
 ###################################################################################################################################################################################################################################################################################################
-# Load data                                                                                                                           #
-selected_genes_Stage_I_data       <-read.table(file = selected_genes_Stage_I_file, sep = '\t', header = TRUE,fill=TRUE)               #
-selected_genes_Stage_II_data      <-read.table(file = selected_genes_Stage_II_file, sep = '\t', header = TRUE,fill=TRUE)              #
-selected_genes_Stage_III_data     <-read.table(file = selected_genes_Stage_III_file, sep = '\t', header = TRUE,fill=TRUE)             #
-                                                                                                                                      #
-# Set rownames                                                                                                                        #
-rownames(selected_genes_Stage_I_data)<-selected_genes_Stage_I_data$Gene                                                               #
-rownames(selected_genes_Stage_II_data)<-selected_genes_Stage_II_data$Gene                                                             #
-rownames(selected_genes_Stage_III_data)<-selected_genes_Stage_III_data$Gene                                                           # 
-
-# Calculate unique genes
-intersect_I   <-intersect(intersect(selected_genes_Stage_I_data$Gene,selected_genes_Stage_I_data$Gene),selected_genes_Stage_III_data$Gene)
-intersect_II  <-intersect(selected_genes_Stage_I_data$Gene,selected_genes_Stage_II_data$Gene)
-intersect_III <-intersect(selected_genes_Stage_I_data$Gene,selected_genes_Stage_III_data$Gene)
-intersect_IV  <-intersect(selected_genes_Stage_II_data$Gene,selected_genes_Stage_III_data$Gene)
-
-write_tsv(data.frame(Gene=intersect_I),      "/home/felipe/Documentos/LungPortal/output/intersection_genes_pos_Stages_I_II_III.tsv")
-write_tsv(data.frame(Gene=intersect_II),     "/home/felipe/Documentos/LungPortal/output/intersection_genes_pos_Stages_I_II.tsv")
-write_tsv(data.frame(Gene=intersect_III),    "/home/felipe/Documentos/LungPortal/output/intersection_genes_pos_Stages_I_III.tsv")
-write_tsv(data.frame(Gene=intersect_IV),     "/home/felipe/Documentos/LungPortal/output/intersection_genes_pos_Stages_II_III.tsv")
+# Load data                                                                                                                                  #
+selected_genes_Stage_I_data       <-read.table(file = selected_genes_Stage_I_file, sep = '\t', header = TRUE,fill=TRUE)                      #
+selected_genes_Stage_II_data      <-read.table(file = selected_genes_Stage_II_file, sep = '\t', header = TRUE,fill=TRUE)                     #
+selected_genes_Stage_III_data     <-read.table(file = selected_genes_Stage_III_file, sep = '\t', header = TRUE,fill=TRUE)                    #
+                                                                                                                                             # 
+# Set rownames                                                                                                                               #
+rownames(selected_genes_Stage_I_data)<-selected_genes_Stage_I_data$Gene                                                                      #
+rownames(selected_genes_Stage_II_data)<-selected_genes_Stage_II_data$Gene                                                                    #
+rownames(selected_genes_Stage_III_data)<-selected_genes_Stage_III_data$Gene                                                                  # 
+                                                                                                                                             #
+# Calculate unique genes                                                                                                                     #
+intersect_I   <-intersect(intersect(selected_genes_Stage_I_data$Gene,selected_genes_Stage_I_data$Gene),selected_genes_Stage_III_data$Gene)   #
+intersect_II  <-intersect(selected_genes_Stage_I_data$Gene,selected_genes_Stage_II_data$Gene)                                                #
+intersect_III <-intersect(selected_genes_Stage_I_data$Gene,selected_genes_Stage_III_data$Gene)                                               #
+intersect_IV  <-intersect(selected_genes_Stage_II_data$Gene,selected_genes_Stage_III_data$Gene)                                              #
+                                                                                                                                             #
+write_tsv(data.frame(Gene=intersect_I),      "/home/felipe/Documentos/LungPortal/output/intersection_genes_pos_Stages_I_II_III.tsv")         #
+write_tsv(data.frame(Gene=intersect_II),     "/home/felipe/Documentos/LungPortal/output/intersection_genes_pos_Stages_I_II.tsv")             #
+write_tsv(data.frame(Gene=intersect_III),    "/home/felipe/Documentos/LungPortal/output/intersection_genes_pos_Stages_I_III.tsv")            #
+write_tsv(data.frame(Gene=intersect_IV),     "/home/felipe/Documentos/LungPortal/output/intersection_genes_pos_Stages_II_III.tsv")           #
 ###################################################################################################################################################################################################################################################################################################

5d8e118c0bcabbd59bd28255d7bb7d6ecfea2a30
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-15 08:07:22 -0500

    Update Pipeline_SquamousCellCarcinoma_CreateExpressionPanel.R

--- Pipeline_SquamousCellCarcinoma_CreateExpressionPanel.R
+++ Pipeline_SquamousCellCarcinoma_CreateExpressionPanel.R
@@ -22,4 +22,26 @@ sel_genes_stage_I   <-selected_genes_Stage_I_data[order(-selected_genes_Stage_I_
 sel_genes_stage_II  <-selected_genes_Stage_II_data[order(-selected_genes_Stage_II_data$log2FoldChange),][1:3,]                        #
 sel_genes_stage_III <-selected_genes_Stage_III_data[order(-selected_genes_Stage_III_data$log2FoldChange),][1:3,]                      #
 #######################################################################################################################################
-
+# Genes of each stage stored in colData                                                                                               #
+sample_stage_I  <-colData[colData$stages=="Stage I","patient_id"]                                                                     #
+sample_stage_II <-colData[colData$stages=="Stage II","patient_id"]                                                                    #
+sample_stage_III<-colData[colData$stages=="Stage III","patient_id"]                                                                   #
+#######################################################################################################################################
+# Filter to keep only positive tumor/normal samples from unstranded_data.                                                             #
+unstranded_data<-unstranded_data[avg_expression_pos$Gene,]                                                                            #
+                                                                                                                                      #
+# omit NA values from vector                                                                                                          #
+unstranded_data <- na.omit(unstranded_data)                                                                                           #
+                                                                                                                                      #
+# Filter to keep only positive tumor/normal samples from colData_data.                                                                #
+colData_data<-colData_data[colnames(unstranded_data),]                                                                                #
+                                                                                                                                      #                                                                                                                                      #
+# Create dds_stages with selected for the purpose of getting normalized counts                                                        #
+dds_stages <- DESeqDataSetFromMatrix(countData = unstranded_data, colData=colData, design = ~  age_at_index +  gender +tissue_type  ) #
+                                                                                                                                      #
+# Estimate size factor                                                                                                                #
+dds_stages <- estimateSizeFactors(dds_stages)                                                                                         #
+                                                                                                                                      #
+# Obtain normalized coutns                                                                                                            #
+norm_counts<-counts(dds_stages, normalized = TRUE)                                                                                    #
+#######################################################################################################################################

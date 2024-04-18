435d03e89cfbde115b47d53e79d8dac9e7af1317
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-18 07:37:23 -0500

    Update Pipeline_SquamousCellCarcinoma_CreateDEGenesPerStage.R

--- Pipeline_SquamousCellCarcinoma_CreateDEGenesPerStage.R
+++ Pipeline_SquamousCellCarcinoma_CreateDEGenesPerStage.R
@@ -14,7 +14,7 @@ avg_expression_pos                 <-read.table(file = avg_expression_pos_file, 
 rownames(colData)                  <-colData$patient_id                                                                   #
 ###########################################################################################################################
 # Filter to only positive tumor/normal samples.
-unstranded_data<-unstranded_data[avg_expression_pos$Gene,]
+#unstranded_data<-unstranded_data[avg_expression_pos$Gene,]
 
 #omit NA values from vector
 unstranded_data <- na.omit(unstranded_data)
@@ -72,6 +72,9 @@ for (stage_index in stages_str)
 #	df_stages[intersect(which(df_stages$log2FoldChange >= 0),which(abs(df_stages$log2FoldChange)>=log2fc_threshold)),"Category"] <-"Up-regulated"
 #	df_stages[intersect(which(df_stages$log2FoldChange <0  ),which(abs(df_stages$log2FoldChange)>=log2fc_threshold)),"Category"] <-"Down-regulated"
 	df_stages[intersect(which(df_stages$log2FoldChange >= 0),which(df_stages$log2FoldChange>=log2fc_threshold)),"Category"] <-"Up-regulated"
+
+	# df_stages
+	df_stages<-df_stages[rownames(df_stages) %in% avg_expression_pos$Gene,]
   	####################################################################################################################	
 	# Save TSV file with genes from Stage3
 	write_tsv(cbind(data.frame(Gene=rownames(df_stages)),df_stages), paste(output_dir,"genes_Stage_",stage_index,".tsv",sep=""))

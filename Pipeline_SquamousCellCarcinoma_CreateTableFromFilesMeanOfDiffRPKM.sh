4cbc8aaa3dfb96ca0828750af393b50ed9db0f3d
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-19 10:24:11 -0500

    Update Pipeline_SquamousCellCarcinoma_CreateTableFromFilesMeanOfDiffRPKM.sh

--- Pipeline_SquamousCellCarcinoma_CreateTableFromFilesMeanOfDiffRPKM.sh
+++ Pipeline_SquamousCellCarcinoma_CreateTableFromFilesMeanOfDiffRPKM.sh
@@ -7,32 +7,55 @@
 # fpkm_unstranded.rna_seq.augmented_star_gene_counts.tsv
 # header.txt ENSG gene id
 # gene_name.txt gene symbol
-
 # A script to obtain all expression data into a data table
-input_folder= /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/
+input_folder="/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/"
 
-# gene_id	            #1
-# gene_name	            #2  
-# gene_typec	        #3
-# unstranded	        #4	
-# stranded_first	    #5
-# stranded_second	    #6	
-# tpm_unstranded	    #7	
-#fpkm_unstranded	    #6
 # List of files fo gene id
-rm /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/all_samples.RPKM.tsv
+rm -f /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageI/all_samples.RPKM.tsv
+rm -f  /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageI/cases.txt
+rm -r /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageI/cases_files.txt
 
 # For each file
-ls /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/*.csv  | while read file
+ls /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageI/*.csv  | while read file
 do
 	case_id=$(cat $file |grep -v "gene_id" | head -n 1 | sed 's\.,,\\g')
-	echo $case_id
-	echo $case_id > "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/"$case_id".RPKM.tsv"
-	cat $file | grep -v "gene_id" | grep -v ,, | sed 's/,/\t/g' >> "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/"$case_id".RPKM.tsv"
+	echo $case_id >> "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageI/cases.txt" 
+	echo -e $case_id > "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageI/"$case_id".RPKM.tsv"
+	cat $file | grep -v "gene_id" | grep -v ,, | sed 's/,/\t/g' >> "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageI/"$case_id".RPKM.tsv"
+ 	echo "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageI/"$case_id".RPKM.tsv" >> "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageI/cases_files.txt"
+done
+
+# List of files fo gene id
+rm -f /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageII/all_samples.RPKM.tsv
+rm -f  /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageII/cases.txt
+rm -r /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageII/cases_files.txt
+# For each file
+ls /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageII/*.csv  | while read file
+do
+	case_id=$(cat $file |grep -v "gene_id" | head -n 1 | sed 's\.,,\\g')
+	echo $case_id >> "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageII/cases.txt" 
+	echo -e $case_id > "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageII/"$case_id".RPKM.tsv"
+	cat $file | grep -v "gene_id" | grep -v ,, | sed 's/,/\t/g' >> "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageII/"$case_id".RPKM.tsv"
+ 	echo "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageII/"$case_id".RPKM.tsv" >> "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageII/cases_files.txt"
+done
+
+# List of files fo gene id
+rm -f /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageIII/all_samples.RPKM.tsv
+rm -f  /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageIII/cases.txt
+rm -r /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageIII/cases_files.txt
+# For each file
+ls /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageIII/*.csv  | while read file
+do
+	case_id=$(cat $file |grep -v "gene_id" | head -n 1 | sed 's\.,,\\g')
+	echo $case_id >> "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageIII/cases.txt" 
+	echo -e $case_id > "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageIII/"$case_id".RPKM.tsv"
+	cat $file | grep -v "gene_id" | grep -v ,, | sed 's/,/\t/g' >> "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageIII/"$case_id".RPKM.tsv"
+ 	echo "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageIII/"$case_id".RPKM.tsv" >> "/home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/stageIII/cases_files.txt"
 done
 
 
-# Finish here, to do
-# Add collumn to the table
-# Check order of gene_ids
-paste /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/*.tsv  > /home/felipe/Documentos/LungPortal/samples/RPKM_by_Carels/all_samples/all_samples.RPKM.tsv
+
+
+
+
+

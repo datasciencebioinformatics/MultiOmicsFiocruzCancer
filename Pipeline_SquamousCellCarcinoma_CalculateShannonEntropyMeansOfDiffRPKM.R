481df4482b96eca4750d44734cbf69233ca78dea
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-24 07:55:41 -0500

    Update Pipeline_SquamousCellCarcinoma_CalculateShannonEntropyMeansOfDiffRPKM.R

--- Pipeline_SquamousCellCarcinoma_CalculateShannonEntropyMeansOfDiffRPKM.R
+++ Pipeline_SquamousCellCarcinoma_CalculateShannonEntropyMeansOfDiffRPKM.R
@@ -4,9 +4,9 @@ library(DescTools)
 list_of_genes<-unique(merge_interactome_gene_symbol[,c("gene_id","PPI")])
 
 # File path
-file_genes_Stage_I   <- "/home/felipe/Documentos/LungPortal/samples/log2foldchange_stageI_pos.tsv"
-file_genes_Stage_II  <- "/home/felipe/Documentos/LungPortal/samples/log2foldchange_stageII_pos.tsv"
-file_genes_Stage_III <- "/home/felipe/Documentos/LungPortal/samples/log2foldchange_stageIII_pos.tsv"
+file_genes_Stage_I   <- "/home/felipe/Documentos/LungPortal/output/DE_GenesPerStageMeansFromPairedUp_Stage_sample_stage_I.tsv"
+file_genes_Stage_II   <- "/home/felipe/Documentos/LungPortal/output/DE_GenesPerStageMeansFromPairedUp_Stage_sample_stage_II.tsv"
+file_genes_Stage_III   <- "/home/felipe/Documentos/LungPortal/output/DE_GenesPerStageMeansFromPairedUp_Stage_sample_stage_III.tsv"
 
 # Gene table
 genes_Stage_I       <-read.table(file = file_genes_Stage_I, sep = '\t', header = TRUE,fill=TRUE)         #
@@ -14,9 +14,9 @@ genes_Stage_II      <-read.table(file = file_genes_Stage_II, sep = '\t', header 
 genes_Stage_III     <-read.table(file = file_genes_Stage_III, sep = '\t', header = TRUE,fill=TRUE)                 #
 
 
-genes_Stage_I<-list_of_genes[list_of_genes$gene %in% genes_Stage_I$Gene,]
-genes_Stage_II<-list_of_genes[list_of_genes$gene %in% genes_Stage_II$Gene,]
-genes_Stage_III<-list_of_genes[list_of_genes$gene %in% genes_Stage_III$Gene,]
+genes_Stage_I<-list_of_genes[list_of_genes$gene %in% genes_Stage_I$gene,]
+genes_Stage_II<-list_of_genes[list_of_genes$gene %in% genes_Stage_II$gene,]
+genes_Stage_III<-list_of_genes[list_of_genes$gene %in% genes_Stage_III$gene,]
 
 round(Entropy(genes_Stage_I$PPI, base = 2),3)
 round(Entropy(genes_Stage_II$PPI, base = 2),3)

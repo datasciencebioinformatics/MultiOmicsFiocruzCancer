d5059ef37b389984e3f6e7bc1d8c17f7aafaf59b
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-24 07:29:50 -0500

    Update Pipeline_SquamousCellCarcinoma_CalculateShannonEntropyMeansOfDiffRPKM.R

--- Pipeline_SquamousCellCarcinoma_CalculateShannonEntropyMeansOfDiffRPKM.R
+++ Pipeline_SquamousCellCarcinoma_CalculateShannonEntropyMeansOfDiffRPKM.R
@@ -4,9 +4,9 @@ library(DescTools)
 list_of_genes<-unique(merge_interactome_gene_symbol[,c("gene_id","PPI")])
 
 # File path
-file_genes_Stage_I   <-"/home/felipe/Documentos/LungPortal/samples/log2foldchange_stageI_pos"
-file_genes_Stage_II  <-"/home/felipe/Documentos/LungPortal/samples/log2foldchange_stageII_pos"
-file_genes_Stage_III <-"/home/felipe/Documentos/LungPortal/samples/log2foldchange_stageIII_pos"
+file_genes_Stage_I   <- "/home/felipe/Documentos/LungPortal/samples/log2foldchange_stageI_pos.tsv"
+file_genes_Stage_II  <- "/home/felipe/Documentos/LungPortal/samples/log2foldchange_stageII_pos.tsv"
+file_genes_Stage_III <- "/home/felipe/Documentos/LungPortal/samples/log2foldchange_stageIII_pos.tsv"
 
 # Gene table
 genes_Stage_I       <-read.table(file = file_genes_Stage_I, sep = '\t', header = TRUE,fill=TRUE)         #

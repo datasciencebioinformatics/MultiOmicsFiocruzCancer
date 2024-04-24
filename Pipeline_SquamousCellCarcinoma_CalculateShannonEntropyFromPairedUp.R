6c778e478a857cee56a2258ba2cb61af65858ab3
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-24 11:37:40 -0500

    Create Pipeline_SquamousCellCarcinoma_CalculateShannonEntropyFromPairedUp.R

---
+++ Pipeline_SquamousCellCarcinoma_CalculateShannonEntropyFromPairedUp.R
@@ -0,0 +1,23 @@
+library(DescTools)
+
+# Take stageI_list_of_genes
+list_of_genes<-unique(merge_interactome_gene_symbol[,c("gene_id","PPI")])
+
+# File path
+file_genes_Stage_I   <-   "/home/felipe/Documentos/LungPortal/output/DE_GenesPerStageMeansFromPairedUp_unique_stage_I.tsv"
+file_genes_Stage_II   <-  "/home/felipe/Documentos/LungPortal/output/DE_GenesPerStageMeansFromPairedUp_unique_stage_II.tsv"
+file_genes_Stage_III   <- "/home/felipe/Documentos/LungPortal/output/DE_GenesPerStageMeansFromPairedUp_unique_stage_III.tsv"
+
+# Gene table
+genes_Stage_I       <-read.table(file = file_genes_Stage_I, sep = '\t', header = TRUE,fill=TRUE)         #
+genes_Stage_II      <-read.table(file = file_genes_Stage_II, sep = '\t', header = TRUE,fill=TRUE)#
+genes_Stage_III     <-read.table(file = file_genes_Stage_III, sep = '\t', header = TRUE,fill=TRUE)                 #
+
+
+genes_Stage_I<-list_of_genes[list_of_genes$gene %in% genes_Stage_I$gene,]
+genes_Stage_II<-list_of_genes[list_of_genes$gene %in% genes_Stage_II$gene,]
+genes_Stage_III<-list_of_genes[list_of_genes$gene %in% genes_Stage_III$gene,]
+
+round(Entropy(genes_Stage_I$PPI, base = 2),3)
+round(Entropy(genes_Stage_II$PPI, base = 2),3)
+round(Entropy(genes_Stage_III$PPI, base = 2),3)

dcb25c5ab10033c2b0f76014c1aaf8fff3b8d38d
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-26 14:15:47 -0500

    Update Pipeline_SquamousCellCarcinoma_CalculateShannonEntropyFromPairedUp.R

--- Pipeline_SquamousCellCarcinoma_CalculateShannonEntropyFromPairedUp.R
+++ Pipeline_SquamousCellCarcinoma_CalculateShannonEntropyFromPairedUp.R
@@ -1,13 +1,21 @@
 library(DescTools)
 
-# Interactome
-interactome_file<-"/home/felipe/Documentos/LungPortal/H-I-05.tsv"
+# Interactome file
+interactome_file<-"/home/felipe/Documentos/LungPortal/Full_Interactome_Flavia.txt"
+
+# EnsemblToUniprotKBconversionList file
+EnsemblToUniprotKBconversionList_file<-"/home/felipe/Documentos/LungPortal/EnsemblToUniprotKBconversionList.txt"
 
 # Gene table
-interactome_data <-read.table(file = interactome_file, sep = '\t', header = FALSE,fill=TRUE)         #
+interactome_data <-read.table(file = interactome_file, sep = '\t', header = TRUE,fill=TRUE)         #
+
+# Gene EnsemblToUniprotKBconversionList_data
+EnsemblToUniprotKBconversionList_data <-read.table(file = EnsemblToUniprotKBconversionList_file, sep = '\t', header = TRUE,fill=TRUE)         #
 
 # Rename collumns 
 colnames(interactome_data)<-c("Gene1","Gene2")
+#######################################################################################################################################
+
 #######################################################################################################################################
 # Take stageI_list_of_genes
 list_of_genes<-unique(merge_interactome_gene_symbol[,c("gene_id","PPI")])
@@ -46,6 +54,17 @@ for (gene_id in genes_Stage_III$gene)
   genes_id_vector_stage_III<-c(genes_id_vector_stage_III,strsplit(gene_id, split = "\\.")[[1]][1])
 }
 ########################################################################################################################################
+# Filter tables
+interactome_data<-interactome_data[interactome_data$Gene1 %in% EnsemblToUniprotKBconversionList_data$SYMBOL,]
+interactome_data<-interactome_data[interactome_data$Gene2 %in% EnsemblToUniprotKBconversionList_data$SYMBOL,]
+
+# Conversion table
+gene1_conversion<-merge(interactome_data,EnsemblToUniprotKBconversionList_data,by.x="Gene1", by.y="SYMBOL",all.x=TRUE,all.y=FALSE)
+gene_conversion<-merge(gene1_conversion,EnsemblToUniprotKBconversionList_data,by.x="Gene2", by.y="SYMBOL",all.x=TRUE,all.y=FALSE)
+
+interactome_data<-gene_conversion[,3:4]
+colnames(interactome_data)<-c("Gene1","Gene2")
+      
 # Store genes
 genes<-unique(c(interactome_data$Gene1,interactome_data$Gene2))
 
@@ -55,25 +74,26 @@ genes_interactome_stage_II <-genes_id_vector_stage_II[genes_id_vector_stage_II %
 genes_interactome_stage_III<-genes_id_vector_stage_III[genes_id_vector_stage_III %in% genes]
 
 # Gene pairs stage I
-gene_starge_I<- unique(interactome_data[c(which(interactome_data$Gene1 %in% genes_interactome_stage_I),
-which(interactome_data$Gene2 %in% genes_interactome_stage_I)),])
+gene_starge_I<- gene_starge_I[gene_starge_I$Gene1 %in% genes_interactome_stage_I,]
+gene_starge_I<- gene_starge_I[gene_starge_I$Gene2 %in% genes_interactome_stage_I,]
 
 # Gene pairs stage II
-gene_starge_II<- unique(interactome_data[c(which(interactome_data$Gene2 %in% genes_interactome_stage_II),
-which(interactome_data$Gene2 %in% genes_interactome_stage_II)),])
+gene_starge_II<- gene_starge_II[gene_starge_II$Gene1 %in% genes_interactome_stage_II,]
+gene_starge_II<- gene_starge_II[gene_starge_II$Gene2 %in% genes_interactome_stage_II,]
 
 # Gene pairs stage III
-gene_starge_III<- unique(interactome_data[c(which(interactome_data$Gene2 %in% genes_interactome_stage_III),
-which(interactome_data$Gene2 %in% genes_interactome_stage_III)),])
+gene_starge_III<- gene_starge_III[gene_starge_III$Gene1 %in% genes_interactome_stage_III,]
+gene_starge_III<- gene_starge_III[gene_starge_III$Gene2 %in% genes_interactome_stage_III,]
 
 df_stageI_connectivity   <-data.frame(Conectivity=table(c(gene_starge_I$Gene1,gene_starge_I$Gene2)))
 df_stageII_connectivity  <-data.frame(Conectivity=table(c(gene_starge_II$Gene1,gene_starge_I$Gene2)))
 df_stageIII_connectivity <-data.frame(Conectivity=table(c(gene_starge_III$Gene1,gene_starge_I$Gene2)))
 
-genes_Stage_I<-list_of_genes[list_of_genes$gene %in% genes_Stage_I$gene,]
-genes_Stage_II<-list_of_genes[list_of_genes$gene %in% genes_Stage_II$gene,]
-genes_Stage_III<-list_of_genes[list_of_genes$gene %in% genes_Stage_III$gene,]
-
 round(Entropy(df_stageI_connectivity$Conectivity.Freq, base = 2),3)
 round(Entropy(df_stageII_connectivity$Conectivity.Freq, base = 2),3)
 round(Entropy(df_stageIII_connectivity$Conectivity.Freq, base = 2),3)
+
+# Save TSV file with genes from Stage1
+write_tsv(df_stageI_connectivity, paste(output_dir,"df_stageI_connectivity_I",".tsv",sep=""))
+write_tsv(df_stageII_connectivity, paste(output_dir,"df_stageI_connectivity_II",".tsv",sep=""))
+write_tsv(df_stageIII_connectivity, paste(output_dir,"df_stageI_connectivity_II",".tsv",sep=""))

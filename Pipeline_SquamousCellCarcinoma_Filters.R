edd156c1803b0903dcc68f9588d9ce6f80d3f4a8
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-28 06:20:56 -0500

    Update Pipeline_SquamousCellCarcinoma_Filters.R

--- Pipeline_SquamousCellCarcinoma_Filters.R
+++ Pipeline_SquamousCellCarcinoma_Filters.R
@@ -8,8 +8,8 @@ merged_data_patient_info_data      <-read.table(file = merged_data_patient_info_
 colData_data                       <-read.table(file = colData_file, sep = '\t', header = TRUE,fill=TRUE)                 #
 rownames(colData)                  <-colData$patient_id                                                                   #
 ###########################################################################################################################
-tumor_samples<-colData[colData$tissue_type=="Tumor","patient_id"]
-normal_samples<-colData[colData$tissue_type=="Normal","patient_id"]
+tumor_samples<-colData_data[colData_data$tissue_type=="Tumor","patient_id"]
+normal_samples<-colData_data[colData_data$tissue_type=="Normal","patient_id"]
 
 # Filter by rowmeans(tumor)-rowmeans(normal)
 unstranded_data_filter<-unstranded_data[rowMeans(unstranded_data[,tumor_samples])-rowMeans(unstranded_data[,normal_samples])>0,]

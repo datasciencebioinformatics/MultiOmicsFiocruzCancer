bfcdd0b2eb6c3302c1b2de9f8bae5cf847eb3757
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-30 13:16:40 -0500

    Update Pipeline_SquamousCellCarcinoma_AssessShannonEntropy.R

--- Pipeline_SquamousCellCarcinoma_AssessShannonEntropy.R
+++ Pipeline_SquamousCellCarcinoma_AssessShannonEntropy.R
@@ -1,20 +1,34 @@
 #######################################################################################################################################
-N=ceiling((length(genes_Stage_I$gene)+length(genes_Stage_II$gene)+length(genes_Stage_III$gene))/3)
+g<-graph_from_data_frame(interactome_data, directed = TRUE, vertices = NULL)
+gI<-graph_from_data_frame(interactome_dataI, directed = TRUE, vertices = NULL)
+gII<-graph_from_data_frame(interactome_dataII, directed = TRUE, vertices = NULL)
+
+interactions_stage_1<-plot(g, vertex.label= NA, edge.arrow.size=0.02,vertex.size = 0.5, xlab = "Network for genes of stage I")
+#######################################################################################################################################
+# Number of genes
+g <- barabasi.game(n=length(genes$gene),m=dim(interactome_data)[1]/length(genes$gene), directed = FALSE, start.graph =g)
+as_data_frame(g, what = c("edges"))
+interactions_random_1<-plot(g, vertex.label= NA, edge.arrow.size=0.02,vertex.size = 0.5, xlab = "Random network for stage I")
+#######################################################################################################################################
+N=ceiling((length(genes$gene)+length(genesI$gene)+length(genesII$gene))/3)
 #######################################################################################################################################
 # entropy_bootstrapping_stage_values
-entropy_bootstrapping_random_Carels<-c()
+entropy_bootstrapping_random<-c()
+entropy_bootstrapping_randomI<-c()
+entropy_bootstrapping_randomII<-c()
 
 # Repeat 1000 times
 for (bootstrapping in 1:1000)
 {
 	print(bootstrapping)
 	print(Sys.time()) # get start time
-
-	# Generate random graph
-	g <- barabasi.game(n=N, directed = FALSE)
-	df_random_graph<-as_data_frame(g, what = c("edges"))
 	########################################################################################################################################
-	df_random_connectivity   <-unique(data.frame(Conectivity=table(c(df_random_graph$from,df_random_graph$to))))	
+	# Generate random graph
+	g_random <- barabasi.game(n=N,m=5, directed = FALSE)
+	########################################################################################################################################
+	g_random<-as_data_frame(g_random, what = c("edges"))
+	########################################################################################################################################	
+	df_random_connectivity   <-unique(data.frame(Conectivity=table(c(g_random$from,g_random$to))))	
 	########################################################################################################################################
 	colnames(df_random_connectivity)<-c("Gene","Conectivity")	
 	########################################################################################################################################
@@ -34,28 +48,24 @@ for (bootstrapping in 1:1000)
 	df_entropy_calulation_random$p_k_mult_log2_pk<-df_entropy_calulation_random$p_k*df_entropy_calulation_random$log2_pk
 	
 	# Caclulate entropy value
-	Entropy_stage_random_value_Carels  <-abs(sum(df_entropy_calulation_random$p_k_mult_log2_pk))
+	Entropy_stage_random_value    <-abs(sum(df_entropy_calulation_random$p_k_mult_log2_pk))
 
 	# Random entropy 
-	entropy_bootstrapping_random_Carels<-c(entropy_bootstrapping_random_Carels,Entropy_stage_random_value_Carels)		
+	entropy_bootstrapping_random<-c(entropy_bootstrapping_random,Entropy_stage_random_value)		
 }
 # Save stages
-df_enropy_stage_all  <-data.frame(1:1000,entropy=entropy_bootstrapping_random_Carels,Method="Conforte")
+df_enropy  <-data.frame(1:1000,entropy=entropy_bootstrapping_random,Method="Conforte")
 
 # Create plot
-plot_enropy_stage_all<-ggplot(df_enropy_stage_all, aes(x=entropy))  + geom_histogram() 
-
-# Histogram overlaid with kernel density curve
-plot_enropy_stage_all     <-plot_enropy_stage_all +  geom_segment(aes(x=Entropy_stage_I_value_Carels, y=200, xend=Entropy_stage_I_value_Carels, yend=0), arrow = arrow(length=unit(0.5, 'cm'))) +   geom_segment(aes(x=Entropy_stage_II_value_Carels, y=200, xend=Entropy_stage_II_value_Carels, yend=0), arrow = arrow(length=unit(0.5, 'cm'))) +  geom_segment(aes(x=Entropy_stage_III_value_Carels, y=200, xend=Entropy_stage_III_value_Carels, yend=0), arrow = arrow(length=unit(0.5, 'cm'))) + theme_bw()
-plot_enropy_stage_all     <-plot_enropy_stage_all + annotate("text", x = Entropy_stage_I_value_Carels, y = 205, label = "Stage I")
-plot_enropy_stage_all     <-plot_enropy_stage_all + annotate("text", x = Entropy_stage_II_value_Carels, y = 205, label = "Stage II") 
-plot_enropy_stage_all     <- plot_enropy_stage_all + annotate("text", x = Entropy_stage_III_value_Carels, y = 205, label = "Stage III")
+plot_enropy  <-ggplot(df_enropy, aes(x=entropy))  + geom_histogram() +  geom_segment(aes(x=Entropy_value_Carels, y=200, xend=Entropy_value_Carels, yend=0), arrow = arrow(length=unit(0.5, 'cm'))) 
+plot_enropyI <-ggplot(df_enropyI, aes(x=entropy))  + geom_histogram() +  geom_segment(aes(x=EntropyI_value_Carels, y=200, xend=EntropyI_value_Carels, yend=0), arrow = arrow(length=unit(0.5, 'cm'))) 
+plot_enropyII <-ggplot(df_enropyII, aes(x=entropy))  + geom_histogram() +  geom_segment(aes(x=EntropyII_value_Carels, y=200, xend=EntropyII_value_Carels, yend=0), arrow = arrow(length=unit(0.5, 'cm'))) 
 
 # FindClusters_resolution
 png(filename=paste(output_dir,"Entropy_","all_.png",sep=""), width = 16, height = 16, res=600, units = "cm")
 	plot_enropy_stage_all + ggtitle("Entropy bootstrapping (1000x random gene sets)")
 dev.off()
 
-sum(Entropy_stage_I_value_Carels<=entropy_bootstrapping_Carels)/1000
-sum(Entropy_stage_II_value_Carels<=entropy_bootstrapping_Carels)/1000
-sum(Entropy_stage_III_value_Carels<=entropy_bootstrapping_Carels)/1000
+sum(Entropy_value_Carels<=entropy_bootstrapping_Carels)/1000
+sum(EntropyI_value_Carels<=entropy_bootstrapping_Carels)/1000
+sum(EntropyII_value_Carels<=entropy_bootstrapping_Carels)/1000

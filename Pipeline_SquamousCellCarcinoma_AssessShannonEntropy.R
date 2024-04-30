a8625aa170adea128a613d8e839b9406cb0a3092
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-04-30 08:08:40 -0500

    Update Pipeline_SquamousCellCarcinoma_AssessShannonEntropy.R

--- Pipeline_SquamousCellCarcinoma_AssessShannonEntropy.R
+++ Pipeline_SquamousCellCarcinoma_AssessShannonEntropy.R
@@ -3,139 +3,73 @@
 n_of_interactions<-ceiling((length(genes_interactome_stage_I)+ length(genes_interactome_stage_II)+length(genes_interactome_stage_II))/3)
 
 # entropy_bootstrapping_stage_values
-entropy_bootstrapping_Stage_I_Carels<-c()
-entropy_bootstrapping_Stage_II_Carels<-c()
-entropy_bootstrapping_Stage_III_Carels<-c()
+entropy_bootstrapping_random_Carels<-c()
 
 # Repeat 1000 times
 for (bootstrapping in 1:1000)
 {
 	print(bootstrapping)
 	print(Sys.time()) # get start time
-	
-	# Store genes stage I, II and III
-	# Vectors to store gene ids from each stage
-	genes_id_vector_random<-sample( rownames(na.omit(unstranded_data_filter)), n_of_interactions, replace = TRUE, prob = NULL)  
 
-	# Vectors to store gene ids from each stage
-	df_correlation_net_stage_I<-t(data.frame(na.omit(unstranded_data_filter[genes_id_vector_random,sample_stage_I])))
-	df_correlation_net_stage_II<-t(data.frame(na.omit(unstranded_data_filter[genes_id_vector_random,sample_stage_II])))
-	df_correlation_net_stage_III<-t(data.frame(na.omit(unstranded_data_filter[genes_id_vector_random,sample_stage_III])))
+	# Data frame of df_random_interacions
+	df_random_interacions<-data.frame(Gene1=c(),Gene2=c())
 
-	net_stage_I <- build_net(df_correlation_net_stage_I, cor_func = "spearman", n_threads =1)
-	net_stage_II <- build_net(df_correlation_net_stage_II, cor_func = "spearman", n_threads =1)
-	net_stage_III <- build_net(df_correlation_net_stage_III, cor_func = "spearman", n_threads =1)
-	
-	# Take the correlation matrix
-	net_stage_I_cor<-net_stage_I$network
-	net_stage_II_cor<-net_stage_II$network
-	net_stage_III_cor<-net_stage_III$network
-	
-	net_stage_I_cor <- melt(net_stage_I_cor)
-	net_stage_II_cor <- melt(net_stage_II_cor)  
-	net_stage_III_cor <- melt(net_stage_III_cor) 
-	
-	# Take value of thhreshold
-	spearman_threshold<-0.99
-	
-	dim(unique(net_stage_I_cor[net_stage_I_cor$value>=spearman_threshold,]))
-	dim(unique(net_stage_II_cor[net_stage_II_cor$value>=spearman_threshold,]))
-	dim(unique(net_stage_III_cor[net_stage_III_cor$value>=spearman_threshold,]))
-	
-	interactions_stage_I<-unique(net_stage_I_cor[net_stage_I_cor$value>=spearman_threshold,c(1,2)])
-	interactions_stage_II<-unique(net_stage_II_cor[net_stage_II_cor$value>=spearman_threshold,c(1,2)])
-	interactions_stage_III<-unique(net_stage_III_cor[net_stage_III_cor$value>=spearman_threshold,c(1,2)])
-
-	colnames(interactions_stage_I)<-c("Gene1","Gene2")
-	colnames(interactions_stage_II)<-c("Gene1","Gene2")
-	colnames(interactions_stage_III)<-c("Gene1","Gene2")
-
-	# copy interactome_data_stages
-	interactome_data_stage_I_clean<-interactions_stage_I
-	interactome_data_stage_II_clean<-interactions_stage_II
-	interactome_data_stage_III_clean<-interactions_stage_III
-	
-	merge_interactome_data<-rbind(data.frame(Gene1=interactome_data_stage_I_clean$Gene1,Gene2=interactome_data_stage_I_clean$Gene2,Stage="Stage I"),
-	data.frame(Gene1=interactome_data_stage_II_clean$Gene1,Gene2=interactome_data_stage_II_clean$Gene2,Stage="Stage II"),
-	data.frame(Gene1=interactome_data_stage_III_clean$Gene1,Gene2=interactome_data_stage_III_clean$Gene2,Stage="Stage III"))
-	
-	# Clean the tables
-	for (gene_pair_index in rownames(merge_interactome_data))
+	# For each interactions
+	for (i in 1:n_of_interactions*(n_of_interactions-1)/2)
 	{
-	    # interactome_data_stage
-	    pair_gene_id_I <-as.vector(merge_interactome_data[gene_pair_index,"Gene1"])
-	    pair_gene_id_II<-as.vector(merge_interactome_data[gene_pair_index,"Gene2"])
-	
-	    # Re-order gene ids
-	    if(pair_gene_id_II<pair_gene_id_I)
-	    {
-	      merge_interactome_data[gene_pair_index,"Gene1"]<-pair_gene_id_II
-	      merge_interactome_data[gene_pair_index,"Gene2"]<-pair_gene_id_I     
-	    }
-	    # Re-order gene ids
-	    if(pair_gene_id_II==pair_gene_id_I)
-	    {
-	      merge_interactome_data[gene_pair_index,"Gene2"]<-"REPEAT"
-	    }
+		# Store genes stage I, II and III
+		# Vectors to store gene ids from each stage
+		node_pair_1<-sample( rownames(na.omit(unstranded_data_filter)), 1, replace = TRUE, prob = NULL) 
+		node_pair_2<-sample( rownames(na.omit(unstranded_data_filter)), 1, replace = TRUE, prob = NULL) 
+		
+		# Re-order gene ids
+		if(pair_gene_id_II<pair_gene_id_I)
+		{
+			node_pair_tmp<-node_pair_1
+			node_pair_1  <-node_pair_2
+			node_pair_2  <-node_pair_1
+		}
+		# Re-order gene ids
+		if(pair_gene_id_II==pair_gene_id_I)
+		{
+			node_pair_2 <- "REPEAT"
+		}	
+		# If probability greater 0.7
+		if (runif(1, min=0, max=1)>0.1)
+		{
+			df_random_interacions<-rbind(df_random_interacions,data.frame(Gene1=node_pair_1,Gene2=node_pair_2))
+		}	
 	}
-	# Take unique values
-	merge_interactome_data<-unique(merge_interactome_data)
+	colnames(df_random_interacions)<-c("Gene1","Gene2")
+
 	########################################################################################################################################
-	interactome_data_stage_I<-merge_interactome_data[merge_interactome_data$Stage=="Stage I",]
-	interactome_data_stage_II<-merge_interactome_data[merge_interactome_data$Stage=="Stage II",]
-	interactome_data_stage_III<-merge_interactome_data[merge_interactome_data$Stage=="Stage III",]
+	df_random_connectivity   <-unique(data.frame(Conectivity=table(c(df_random_interacions$Gene1,df_random_interacions$Gene2))))	
 	########################################################################################################################################
-	interactome_data_stage_I<-unique(interactome_data_stage_I[,c("Gene1","Gene2")])
-	interactome_data_stage_II<-unique(interactome_data_stage_II[,c("Gene1","Gene2")])
-	interactome_data_stage_III<-unique(interactome_data_stage_III[,c("Gene1","Gene2")])
-	########################################################################################################################################
-	df_stageI_connectivity   <-unique(data.frame(Conectivity=table(c(interactome_data_stage_I$Gene1,interactome_data_stage_I$Gene2))))
-	df_stageII_connectivity  <-unique(data.frame(Conectivity=table(c(interactome_data_stage_II$Gene1,interactome_data_stage_II$Gene2))))
-	df_stageIII_connectivity <-unique(data.frame(Conectivity=table(c(interactome_data_stage_III$Gene1,interactome_data_stage_III$Gene2))))
-	########################################################################################################################################
-	colnames(df_stageI_connectivity)<-c("Gene","Conectivity")
-	colnames(df_stageII_connectivity)<-c("Gene","Conectivity")
-	colnames(df_stageIII_connectivity)<-c("Gene","Conectivity")
-	########################################################################################################################################
-	df_stageI_connectivity<-df_stageI_connectivity[df_stageI_connectivity$Gene!="REPEAT",]
-	df_stageII_connectivity<-df_stageII_connectivity[df_stageII_connectivity$Gene!="REPEAT",]
-	df_stageIII_connectivity<-df_stageIII_connectivity[df_stageIII_connectivity$Gene!="REPEAT",]
+	colnames(df_random_connectivity)<-c("Gene","Conectivity")	
 	########################################################################################################################################
 	# Table for the calculation of entropy
-	df_entropy_calulation_I   <-data.frame(table(df_stageI_connectivity$Conectivity),p_k=0,log2_pk=0,p_k_mult_log2_pk=0)
-	df_entropy_calulation_II  <-data.frame(table(df_stageII_connectivity$Conectivity),p_k=0,log2_pk=0,p_k_mult_log2_pk=0)
-	df_entropy_calulation_III <-data.frame(table(df_stageIII_connectivity$Conectivity),p_k=0,log2_pk=0,p_k_mult_log2_pk=0)
+	df_entropy_calulation_random   <-data.frame(table(df_random_connectivity$Conectivity),p_k=0,log2_pk=0,p_k_mult_log2_pk=0)
 	
 	# Rename colnames
-	colnames(df_entropy_calulation_I)<-c("k","count","p_k","log2_pk","p_k_mult_log2_pk")
-	colnames(df_entropy_calulation_II)<-c("k","count","p_k","log2_pk","p_k_mult_log2_pk")
-	colnames(df_entropy_calulation_III)<-c("k","count","p_k","log2_pk","p_k_mult_log2_pk")
-	
+	colnames(df_entropy_calulation_random)<-c("k","count","p_k","log2_pk","p_k_mult_log2_pk")
+		
 	# Calculate p(k)
-	df_entropy_calulation_I$p_k<-df_entropy_calulation_I$count/sum(df_entropy_calulation_I$count)
-	df_entropy_calulation_II$p_k<-df_entropy_calulation_II$count/sum(df_entropy_calulation_II$count)
-	df_entropy_calulation_III$p_k<-df_entropy_calulation_III$count/sum(df_entropy_calulation_III$count)
+	df_entropy_calulation_random$p_k<-df_entropy_calulation_random$count/sum(df_entropy_calulation_random$count)	
 	
 	# Calculate log2(p(k))
-	df_entropy_calulation_I$log2_pk<-log(df_entropy_calulation_I$p_k,2)
-	df_entropy_calulation_II$log2_pk<-log(df_entropy_calulation_II$p_k,2)
-	df_entropy_calulation_III$log2_pk<-log(df_entropy_calulation_III$p_k,2)
-	
+	df_entropy_calulation_random$log2_pk<-log(df_entropy_calulation_random$p_k,2)
+		
 	# Calculate p(k)*log2(p(k))
-	df_entropy_calulation_I$p_k_mult_log2_pk<-df_entropy_calulation_I$p_k*df_entropy_calulation_I$log2_pk
-	df_entropy_calulation_II$p_k_mult_log2_pk<-df_entropy_calulation_II$p_k*df_entropy_calulation_II$log2_pk
-	df_entropy_calulation_III$p_k_mult_log2_pk<-df_entropy_calulation_III$p_k*df_entropy_calulation_III$log2_pk
+	df_entropy_calulation_random$p_k_mult_log2_pk<-df_entropy_calulation_random$p_k*df_entropy_calulation_random$log2_pk
 	
 	# Caclulate entropy value
-	Entropy_stage_I_value_Carels  <-abs(sum(df_entropy_calulation_I$p_k_mult_log2_pk))
-	Entropy_stage_II_value_Carels <-abs(sum(df_entropy_calulation_II$p_k_mult_log2_pk))
-	Entropy_stage_III_value_Carels<-abs(sum(df_entropy_calulation_III$p_k_mult_log2_pk))
+	Entropy_stage_random_value_Carels  <-abs(sum(df_entropy_calulation_random$p_k_mult_log2_pk))
 
-	entropy_bootstrapping_Stage_I_Carels<-c(entropy_bootstrapping_Stage_I_Carels,Entropy_stage_I_value_Carels)
-	entropy_bootstrapping_Stage_II_Carels<-c(entropy_bootstrapping_Stage_II_Carels,Entropy_stage_II_value_Carels)
-	entropy_bootstrapping_Stage_III_Carels<-c(entropy_bootstrapping_Stage_III_Carels,Entropy_stage_III_value_Carels)
-		
+	# Random entropy 
+	entropy_bootstrapping_random_Carels<-c(entropy_bootstrapping_random_Carels,Entropy_stage_random_value_Carels)		
 }
+
+
 # Save stages
 df_enropy_stage_all  <-data.frame(1:1000,entropy=entropy_bootstrapping_Carels,Method="Conforte")
 

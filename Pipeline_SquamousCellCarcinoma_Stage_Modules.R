3185f173d3764af02a020d9a495e47875ac80ab8
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-05-01 07:10:46 -0500

    Update Pipeline_SquamousCellCarcinoma_Stage_Modules.R

--- Pipeline_SquamousCellCarcinoma_Stage_Modules.R
+++ Pipeline_SquamousCellCarcinoma_Stage_Modules.R
@@ -1,4 +1,6 @@
 library(igraph)
+library(ggnet)
+library(network)
 
 # File path to gene stages
 file_genes_Stage_I   <-   paste(output_dir,"df_stageI_interactome",".tsv",sep="")
@@ -11,25 +13,29 @@ df_stageII_interactome      <-read.table(file = file_genes_Stage_II, sep = '\t',
 df_stageIII_interactome     <-read.table(file = file_genes_Stage_III, sep = '\t', header = TRUE,fill=TRUE) 
 
 # File path to gene stages
-g_stage_I<-graph_from_data_frame(df_stageI_interactome, directed = TRUE, vertices = NULL)
-g_stage_II<-graph_from_data_frame(df_stageII_interactome, directed = TRUE, vertices = NULL)
-g_stage_III<-graph_from_data_frame(df_stageIII_interactome, directed = TRUE, vertices = NULL)
+g_stage_I<-graph_from_data_frame(df_stageI_interactome, directed = FALSE, vertices = NULL)
+g_stage_II<-graph_from_data_frame(df_stageII_interactome, directed = FALSE, vertices = NULL)
+g_stage_III<-graph_from_data_frame(df_stageIII_interactome, directed = FALSE, vertices = NULL)
+
+
 
 g_stage_I_ceb <- cluster_edge_betweenness(g_stage_I) 
-dendPlot(g_stage_I_ceb, mode="hclust")
-l <- do.call("layout_with_kk", list(g_stage_I)) 
-plot(g_stage_I_ceb, g_stage_I, layout=l) 
+c_stage_I$names[c_stage_I$membership==1]
 
-# FindClusters_resolution
-png(filename=paste(output_dir,"Network_","Stage_I",".png",sep=""), width = 32, height = 32, res=600, units = "cm")
-  plot(g_stage_I_ceb, g_stage_I, layout=l) 
-dev.off()
+# greedy method (hiearchical, fast method)
+c_stage_I = cluster_fast_greedy(g_stage_I)
+coords = layout_with_fr(g_stage_I)
+plot(c_stage_I, g_stage_I, layout=coords) 
 
 
-layouts <- grep("^layout_", ls("package:igraph"), value=TRUE)[-1] 
-# Remove layouts that do not apply to our graph.
-layouts <- layouts[!grepl("bipartite|merge|norm|sugiyama|tree", layouts)]
-par(mfrow=c(3,3), mar=c(1,1,1,1))
+df_stageI_interactome.net <- network(df_stageI_interactome, directed = FALSE)
+df_stageI_interactome.net %v% "membership" = g_stage_I_ceb$membership
+ggnet2(df_stageI_interactome.net, color = "membership",layout.par = list(cell.jitter = 0.75)) +  coord_equal()
+
+ggnet2(df_stageI_interactome.net, color = membership) +  coord_equal()
+
+
+net %v% "x" = x[, 1]
 
 for (layout in layouts) {
   print(layout)

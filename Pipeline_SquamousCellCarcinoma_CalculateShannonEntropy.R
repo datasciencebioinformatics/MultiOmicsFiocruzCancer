ac6c6829c1e3d15ee97fa941b286afb8f20ae30f
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-05-03 09:30:03 -0500

    Update Pipeline_SquamousCellCarcinoma_CalculateShannonEntropy.R

--- Pipeline_SquamousCellCarcinoma_CalculateShannonEntropy.R
+++ Pipeline_SquamousCellCarcinoma_CalculateShannonEntropy.R
@@ -13,9 +13,9 @@ Table1_data <- Table1_data[match(unique(Table1_data$gene_id), Table1_data$gene_i
 rownames(Table1_data)<-Table1_data$gene_id
 #######################################################################################################################################
 # File path to gene stages
-file_genes_Stage_I   <-   "/home/felipe/Documentos/LungPortal/output/DE_GenesPerStageMeansFromPairedUp_unique_stage_I.tsv"
-file_genes_Stage_II   <-  "/home/felipe/Documentos/LungPortal/output/DE_GenesPerStageMeansFromPairedUp_unique_stage_II.tsv"
-file_genes_Stage_III   <- "/home/felipe/Documentos/LungPortal/output/DE_GenesPerStageMeansFromPairedUp_unique_stage_III.tsv"
+file_genes_Stage_I   <-   paste(output_dir,"DE_GenesPerStageMeansFromPairedUp_unique_stage_I.tsv",sep="")
+file_genes_Stage_II   <-  paste(output_dir,"DE_GenesPerStageMeansFromPairedUp_unique_stage_II.tsv",sep="")
+file_genes_Stage_III   <- paste(output_dir,"DE_GenesPerStageMeansFromPairedUp_unique_stage_III.tsv",sep="")
 
 # Gene table
 genes_Stage_I       <-read.table(file = file_genes_Stage_I, sep = '\t', header = TRUE,fill=TRUE)         

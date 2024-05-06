3f013ed6dbe01b866b137d088f576dc62cee896b
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-05-06 11:59:36 -0500

    Update Pipeline_SquamousCellCarcinoma_NetworkAnalysisPlot.R

--- Pipeline_SquamousCellCarcinoma_NetworkAnalysisPlot.R
+++ Pipeline_SquamousCellCarcinoma_NetworkAnalysisPlot.R
@@ -1,9 +1,8 @@
+###############################################################################################################################
+Network_stages_value_file               <- "/home/felipe/Documentos/Fiocruz/MultiOmicsFiocruzCancer/Network_stages_value.txt" #
+###############################################################################################################################
+Network_stages_value_file_data     <-read.table(file = Network_stages_value_file, sep = '\t', header = TRUE,fill=TRUE)    #
 ###########################################################################################################################
-merged_data_patient_info_file       <- "/home/felipe/Documentos/LungPortal/samples/patient.metadata.tsv"                  #
-colData_file                        <- "/home/felipe/Documentos/LungPortal/samples/colData.tsv"                           #
-###########################################################################################################################
-unstranded_data                    <- unstranded_rpkm
-merged_data_patient_info_data      <-read.table(file = merged_data_patient_info_file, sep = '\t', header = TRUE,fill=TRUE)#
-colData_data                       <-read.table(file = colData_file, sep = '\t', header = TRUE,fill=TRUE)                 #
-rownames(colData)                  <-colData$patient_id                                                                   #
-###########################################################################################################################
+
+# Standard deviation of the mean as error bar
+p <- ggplot(Network_stages_value_file_data, aes(x=Stage, y=Value)) +geom_bar(stat="identity")+ facet_wrap(~Variable, ncol = 5, nrow = 2, scales = "free_y") +theme_bw() + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))

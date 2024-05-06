71ce856e40e1b915af93a87221c4c7a3389f97f1
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-05-06 11:41:09 -0500

    Create Pipeline_SquamousCellCarcinoma_NetworkAnalysisPlot.R

---
+++ Pipeline_SquamousCellCarcinoma_NetworkAnalysisPlot.R
@@ -0,0 +1,9 @@
+###########################################################################################################################
+merged_data_patient_info_file       <- "/home/felipe/Documentos/LungPortal/samples/patient.metadata.tsv"                  #
+colData_file                        <- "/home/felipe/Documentos/LungPortal/samples/colData.tsv"                           #
+###########################################################################################################################
+unstranded_data                    <- unstranded_rpkm
+merged_data_patient_info_data      <-read.table(file = merged_data_patient_info_file, sep = '\t', header = TRUE,fill=TRUE)#
+colData_data                       <-read.table(file = colData_file, sep = '\t', header = TRUE,fill=TRUE)                 #
+rownames(colData)                  <-colData$patient_id                                                                   #
+###########################################################################################################################

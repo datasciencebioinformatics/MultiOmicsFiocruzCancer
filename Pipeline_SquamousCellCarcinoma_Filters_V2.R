ef7c403771c509a2167704d2283e392b49f911b8
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-05-07 11:30:53 -0500

    Create Pipeline_SquamousCellCarcinoma_Filters_V2.R

---
+++ Pipeline_SquamousCellCarcinoma_Filters_V2.R
@@ -0,0 +1,20 @@
+###########################################################################################################################
+merged_data_patient_info_file       <- "/home/felipe/Documentos/LungPortal/samples/patient.metadata.tsv"                  #
+colData_file                        <- "/home/felipe/Documentos/LungPortal/samples/colData.tsv"                           #
+###########################################################################################################################
+unstranded_data                    <- unstranded_rpkm
+merged_data_patient_info_data      <-read.table(file = merged_data_patient_info_file, sep = '\t', header = TRUE,fill=TRUE)#
+colData_data                       <-read.table(file = colData_file, sep = '\t', header = TRUE,fill=TRUE)                 #
+rownames(colData)                  <-colData$patient_id                                                                   #
+###########################################################################################################################
+log2foldchange_trheshold<-log2foldchange_trheshold
+# log2fc_threshold
+log2fc_threshold<-log2fc_threshold
+
+# Filter by log2folchage
+unstranded_rpkm<-unstranded_rpkm[rownames(log2change_tumor_control[which(log2change_tumor_control$log2change>=log2foldchange_trheshold),]),]
+
+# Filter by RPKM
+unstranded_data_filter<-unstranded_rpkm[rowMeans(unstranded_rpkm[,tumor_samples])>10,]
+###########################################################################################################################
+print(paste("Number of up-regulated tumor-genes :",dim(unstranded_data_filter)[1]))

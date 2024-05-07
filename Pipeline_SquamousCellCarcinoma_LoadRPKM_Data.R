56f084f81f12732b9f210e8e35c8efb2b8f3ea93
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-05-07 12:08:56 -0500

    Update Pipeline_SquamousCellCarcinoma_LoadRPKM_Data.R

--- Pipeline_SquamousCellCarcinoma_LoadRPKM_Data.R
+++ Pipeline_SquamousCellCarcinoma_LoadRPKM_Data.R
@@ -1,1 +1,1 @@
-unstranded_data  <-read.table(file = paste(output_dir,"unstranded_rpkm.tsv",sep=""), sep = '\t', header = TRUE,)         #
+unstranded_rpkm  <-read.table(file = paste(output_dir,"unstranded_rpkm.tsv",sep=""), sep = '\t', header = TRUE,)         #

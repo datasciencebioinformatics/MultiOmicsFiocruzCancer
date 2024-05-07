14dc7f7e041c12ec36f52c394cad038706caaed5
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-05-07 08:39:17 -0500

    Update Pipeline_SquamousCellCarcinoma_VeenDiagramsFromPairedUp.R

--- Pipeline_SquamousCellCarcinoma_VeenDiagramsFromPairedUp.R
+++ Pipeline_SquamousCellCarcinoma_VeenDiagramsFromPairedUp.R
@@ -25,8 +25,6 @@ unique_stage_III=setdiff(selected_genes_Stage_III_data$gene, c(selected_genes_St
 stages_I_II_III_unique<-ggVennDiagram(list(Stage_I=unique_stage_I,Stage_II=unique_stage_II,Stage_III=unique_stage_III), label_alpha = 0.9,set_color = c("grey50","grey50","grey50")) +  scale_fill_gradient(low = "white", high = "white") + theme_bw() + ggtitle("Stages I, II and III")+ guides(fill="none")
 stages_I_II_III<-ggVennDiagram(list(Stage_I=selected_genes_Stage_I_data$gene,Stage_II=selected_genes_Stage_II_data$gene,Stage_III=selected_genes_Stage_III_data$gene), label_alpha = 0.9,set_color = c("grey50","grey50","grey50")) + scale_fill_gradient(low = "white", high = "white") + theme_bw() + ggtitle("Stages I, II and III, without overlap")+ guides(fill="none")
 
-
-
 # FindClusters_resolution
 png(filename=paste(output_dir,"Ven_Diagrams.png",sep=""), width = 28, height = 14, res=600, units = "cm")
   grid.arrange(stages_I_II_III, stages_I_II_III_unique, nrow = 1)
@@ -35,4 +33,8 @@ dev.off()
 write_tsv(selected_genes_Stage_I_data[unique_stage_I,], paste(output_dir,"DE_GenesPerStageMeansFromPairedUp_unique_stage_I",".tsv",sep=""))
 write_tsv(selected_genes_Stage_II_data[unique_stage_II,], paste(output_dir,"DE_GenesPerStageMeansFromPairedUp_unique_stage_II",".tsv",sep=""))
 write_tsv(selected_genes_Stage_III_data[unique_stage_III,], paste(output_dir,"DE_GenesPerStageMeansFromPairedUp_unique_stage_III",".tsv",sep=""))
+#############################################################################################################################
+print(paste("Number of tumor genes per stage for Stage I : ",paste(length(rownames(selected_genes_Stage_I_data)),"/",length(unique_stage_I),sep="")))
+print(paste("Number of tumor genes per stage for Stage II : ",paste(length(rownames(selected_genes_Stage_II_data)),"/",length(unique_stage_II),sep="")))
+print(paste("Number of tumor genes per stage for Stage III : ",paste(length(rownames(selected_genes_Stage_III_data)),"/",length(unique_stage_III),sep="")))
 #############################################################################################################################

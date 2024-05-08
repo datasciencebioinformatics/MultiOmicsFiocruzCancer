fa4178ae93a72a390615f421ab972b7e4b89091e
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-05-08 08:38:01 -0500

    Update Pipeline_SquamousCellCarcinoma_NetworkAnalysisPlot.R

--- Pipeline_SquamousCellCarcinoma_NetworkAnalysisPlot.R
+++ Pipeline_SquamousCellCarcinoma_NetworkAnalysisPlot.R
@@ -4,10 +4,10 @@ Network_stages_value_file               <- "/home/felipe/Documentos/Fiocruz/Mult
 Network_stages_value_file_data     <-read.table(file = Network_stages_value_file, sep = '\t', header = TRUE,fill=TRUE)    #
 ###########################################################################################################################
 # Relevel factor
-Network_stages_value_file_data$Variable<-factor(Network_stages_value_file_data$Variable,levels=c("Number of nodes","Number of edges","Avg. number of neighbors","Network diameter","Network radius","Characteristic path length","Clustering coefficient","Network density","Network heterogeneity","Network centralization"))
+Network_stages_value_file_data$Variable<-factor(Network_stages_value_file_data$Variable,levels=c("Number of nodes","Number of edges","Avg. number of neighbors","Network diameter","Network radius","Characteristic path length","Clustering coefficient","Network density","Network heterogeneity","Network centralization","Connected components"))
 
 # Standard deviation of the mean as error bar
-p <- ggplot(Network_stages_value_file_data, aes(x=Stage, y=Value)) +geom_bar(stat="identity")+ facet_wrap(~Variable, ncol = 5, nrow = 2, scales = "free_y") +theme_bw() + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+p <- ggplot(Network_stages_value_file_data, aes(x=Stage, y=Value)) +geom_bar(stat="identity")+ facet_wrap(~Variable, ncol = 5, nrow = 3, scales = "free_y") +theme_bw() + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
 # FindClusters_resolution
 png(filename=paste(output_folder,"Network_panel_","summary.png",sep=""), width = 26, height = 16, res=600, units = "cm")

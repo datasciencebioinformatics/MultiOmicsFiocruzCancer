daaff760c5bec8d183612731bdd5c9618ea9a07d
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-05-10 14:17:14 -0500

    Update Pipeline_SquamousCellCarcinoma_CreateDEGenesPerStageMeansFromPairedV2.R

--- Pipeline_SquamousCellCarcinoma_CreateDEGenesPerStageMeansFromPairedV2.R
+++ Pipeline_SquamousCellCarcinoma_CreateDEGenesPerStageMeansFromPairedV2.R
@@ -67,8 +67,8 @@ for (comparisson_index in rownames(df_table_comparisson))
 	Stages_ii_and_iii_sample_expr<-na.omit(unstranded_data[DE_genes,Stages_ii_and_iii_sample])
 	####################################################################################################################
 	# folchange=Expr(Stage i)/Expr(Stage ii and II)
-	folchange=rowMeans(Stage_i_samples_expr)-rowMeans(Stages_ii_and_iii_sample_expr)
-	#folchange=rowMeans(Stage_i_samples_expr)/rowMeans(Stages_ii_and_iii_sample_expr)
+	#folchange=rowMeans(Stage_i_samples_expr)-rowMeans(Stages_ii_and_iii_sample_expr)
+	folchange=rowMeans(Stage_i_samples_expr)/rowMeans(Stages_ii_and_iii_sample_expr)
 	
 	# log2change
 	log2change=log(folchange,2)	

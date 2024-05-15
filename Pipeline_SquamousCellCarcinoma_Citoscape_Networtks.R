a3b180769223e41921b3ee441060c574106e6d63
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-05-15 12:27:01 -0500

    Update Pipeline_SquamousCellCarcinoma_Citoscape_Networtks.R

--- Pipeline_SquamousCellCarcinoma_Citoscape_Networtks.R
+++ Pipeline_SquamousCellCarcinoma_Citoscape_Networtks.R
@@ -3,7 +3,6 @@ library(RCy3)
 library(RColorBrewer)
 # cyrestGET(operation = NULL, parameters = NULL, base.url = "http://localhost:1234")
 ############################################################################################################################################
-
 stage_I_interaction_file               <- "/media/felipe/UBUNTU\ 20_0/Network_cytoscape/df_stageI_interactome_interactome.tsv" #
 stage_II_interaction_file               <- "/media/felipe/UBUNTU\ 20_0/Network_cytoscape/df_stageII_interactome_interactome.tsv" #
 stage_III_interaction_file               <- "/media/felipe/UBUNTU\ 20_0/Network_cytoscape/df_stageIII_interactome_interactome.tsv" #
@@ -19,14 +18,14 @@ merge_interactome_gene_symbol <- merge_interactome_gene_symbol[match(unique(merg
 rownames(merge_interactome_gene_symbol)<-merge_interactome_gene_symbol$gene_id
 
 # Converted gene_ids
-interactome_smbols_stage_I    <-na.omit(data.frame(Gene1=merge_interactome_gene_symbol[interactome_data_stage_I$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[interactome_data_stage_I$Gene2,"gene_symbol"]))
-interactome_smbols_stage_II   <-na.omit(data.frame(Gene1=merge_interactome_gene_symbol[interactome_data_stage_II$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[interactome_data_stage_II$Gene2,"gene_symbol"]))
-interactome_smbols_stage_III  <-na.omit(data.frame(Gene1=merge_interactome_gene_symbol[interactome_data_stage_III$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[interactome_data_stage_III$Gene2,"gene_symbol"]))
+stage_I_interaction_data    <-na.omit(data.frame(Gene1=merge_interactome_gene_symbol[stage_I_interaction_data$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[stage_I_interaction_data$Gene2,"gene_symbol"]))
+stage_II_interaction_data   <-na.omit(data.frame(Gene1=merge_interactome_gene_symbol[stage_II_interaction_data$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[stage_II_interaction_data$Gene2,"gene_symbol"]))
+stage_III_interaction_data  <-na.omit(data.frame(Gene1=merge_interactome_gene_symbol[stage_III_interaction_data$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[stage_III_interaction_data$Gene2,"gene_symbol"]))
 
 # Read igraph
-interactome_igraph_stage_I<-graph_from_data_frame(interactome_smbols_stage_I, directed = FALSE, vertices = NULL)
-interactome_igraph_stage_II<-graph_from_data_frame(interactome_smbols_stage_II, directed = FALSE, vertices = NULL)
-interactome_igraph_stage_III<-graph_from_data_frame(interactome_smbols_stage_III, directed = FALSE, vertices = NULL)
+interactome_igraph_stage_I<-graph_from_data_frame(stage_I_interaction_data, directed = FALSE, vertices = NULL)
+interactome_igraph_stage_II<-graph_from_data_frame(stage_II_interaction_data, directed = FALSE, vertices = NULL)
+interactome_igraph_stage_III<-graph_from_data_frame(stage_III_interaction_data, directed = FALSE, vertices = NULL)
 
 # greedy method (hiearchical, fast method)
 cluster_igraph_stage_I  <- cluster_fast_greedy(interactome_igraph_stage_I)
@@ -37,13 +36,10 @@ cluster_igraph_stage_III <- cluster_fast_greedy(interactome_igraph_stage_III)
 nb.cols  <- max(c(length(unique(membership(cluster_igraph_stage_I))),length(unique(membership(cluster_igraph_stage_II))), length(unique(membership(cluster_igraph_stage_III)))))
 mycolors <- data.frame(colour=colorRampPalette(brewer.pal(8, "Set3"))(nb.cols))
 
-
-
 # Generate colors based on media type:
 V(interactome_igraph_stage_I)$color  <- mycolors[as.numeric( membership(cluster_igraph_stage_I)),]
 V(interactome_igraph_stage_II)$color <- mycolors[as.numeric( membership(cluster_igraph_stage_II)),]
 V(interactome_igraph_stage_III)$color<- mycolors[as.numeric( membership(cluster_igraph_stage_III)),]
-
 
 # Set node size based on audience size:
 V(interactome_igraph_stage_I)$size <- degree(interactome_igraph_stage_I)
@@ -68,7 +64,7 @@ dev.off()
 
 
 # Save TSV file with genes from Stage1
-write_tsv(interactome_smbols_stage_I, paste(output_dir,"interactome_smbols_stage_I",".tsv",sep=""))
-write_tsv(interactome_smbols_stage_II, paste(output_dir,"interactome_smbols_stage_II",".tsv",sep=""))
-write_tsv(interactome_smbols_stage_III, paste(output_dir,"interactome_smbols_stage_III",".tsv",sep=""))
+write_tsv(stage_I_interaction_data, paste(output_dir,"interactome_smbols_stage_I",".tsv",sep=""))
+write_tsv(stage_II_interaction_data, paste(output_dir,"interactome_smbols_stage_II",".tsv",sep=""))
+write_tsv(stage_III_interaction_data, paste(output_dir,"interactome_smbols_stage_III",".tsv",sep=""))
 

0f050fd157f1677dee44c56e17ba23e27c1ca75d
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-05-17 07:18:57 -0500

    Update Pipeline_SquamousCellCarcinoma_CalculateShannonEntropy.R

--- Pipeline_SquamousCellCarcinoma_CalculateShannonEntropy.R
+++ Pipeline_SquamousCellCarcinoma_CalculateShannonEntropy.R
@@ -38,9 +38,9 @@ df_correlation_net_stage_II<-data.frame(na.omit(unstranded_data_filter[genes_Sta
 df_correlation_net_stage_III<-data.frame(na.omit(unstranded_data_filter[genes_Stage_III$gene,]))
 #######################################################################################################################################
 # Filter by low variability
-#df_stage_I_filtered<-filter_low_var(t(df_correlation_net_stage_I), pct = 0.75, type = c("mean"))
-#df_stage_II_filtered<-filter_low_var(t(df_correlation_net_stage_II), pct = 0.75, type = c("mean"))
-#df_stage_III_filtered<-filter_low_var(t(df_correlation_net_stage_III), pct = 0.75, type = c("mean"))
+df_stage_I_filtered<-filter_low_var(t(df_correlation_net_stage_I), pct = 0.75, type = c("mean"))
+df_stage_II_filtered<-filter_low_var(t(df_correlation_net_stage_II), pct = 0.75, type = c("mean"))
+df_stage_III_filtered<-filter_low_var(t(df_correlation_net_stage_III), pct = 0.75, type = c("mean"))
 
 # Set threshold
 upper_weight_th = threshold_cor

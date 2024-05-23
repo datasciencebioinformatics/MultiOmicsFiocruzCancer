60fd40422c05b6bbfe9dd16a4e6b4b5dfba92faf
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-05-23 12:21:30 -0500

    Create BipartiteAnalysis.R

---
+++ BipartiteAnalysis.R
@@ -0,0 +1,20 @@
+##################################################################################################
+# First analysis, co-expression network and sub-interactome network for each stage, together with bipartite networks.
+# Co-expression network for stage I
+# Co-expression network for stage II
+# Co-expression network for stage III
+
+# Sub-interactome network for stage I
+# Sub-interactome network for stage II
+# Sub-interactome network for stage III
+
+# Bi-partite Co-expression vs. Sub-interactome network for stage I
+# Bi-partite Co-expression vs. Sub-interactome network for stage II
+# Bi-partite Co-expression vs. Sub-interactome network for stage III
+################################################################################################
+# Planejamento igraph networks
+# 1- Primeira análise, rede de coexpressão e rede de subinteractomas para cada estágio, juntamente com redes bipartidas.
+# Rede co-expressão Estágio I <-> Bipartite co-expressão com sub-interactoma Estágio I <-> Rede sub-interactoma Estágio I
+
+# 2- Segunda análise, rede para cada estágio, juntamente com redes bipartidas para cada par de estágios
+Rede estágio I- Bipartite estagios I e II - Rede estágio II - bipartites II e III - Rede estágio III

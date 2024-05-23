07e73e71d4391b3c189a9116bf76616d50700f63
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-05-23 13:46:07 -0500

    Update BipartiteAnalysis.R

--- BipartiteAnalysis.R
+++ BipartiteAnalysis.R
@@ -1,4 +1,14 @@
+# Co-expressin network
+coexpression_network_Stage_I   <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_10_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/df_stageI_interactome.tsv"
+coexpression_network_Stage_II  <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_10_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/df_stageII_interactome.tsv"
+coexpression_network_Stage_III <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_10_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/df_stageIII_interactome.tsv"
 ##################################################################################################
+coexpression_network_Stage_I   <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_10_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/df_stageI_interactome.tsv"
+coexpression_network_Stage_II  <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_10_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/df_stageII_interactome.tsv"
+coexpression_network_Stage_III <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_10_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/df_stageIII_interactome.tsv"
+##################################################################################################
+
+
 # First analysis, co-expression network and sub-interactome network for each stage, together with bipartite networks.
 # Co-expression network for stage I
 # Co-expression network for stage II
@@ -15,6 +25,8 @@
 # Planejamento igraph networks
 # 1- Primeira análise, rede de coexpressão e rede de subinteractomas para cada estágio, juntamente com redes bipartidas.
 # Rede co-expressão Estágio I <-> Bipartite co-expressão com sub-interactoma Estágio I <-> Rede sub-interactoma Estágio I
+# Rede co-expressão Estágio II <-> Bipartite co-expressão com sub-interactoma Estágio II <-> Rede sub-interactoma Estágio II
+# Rede co-expressão Estágio III <-> Bipartite co-expressão com sub-interactoma Estágio II <-> Rede sub-interactoma Estágio III
 
 # 2- Segunda análise, rede para cada estágio, juntamente com redes bipartidas para cada par de estágios
 Rede estágio I- Bipartite estagios I e II - Rede estágio II - bipartites II e III - Rede estágio III

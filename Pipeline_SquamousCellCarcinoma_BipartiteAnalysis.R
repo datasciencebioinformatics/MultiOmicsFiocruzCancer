c37f286fb1a857c2e0c30e0bb34adc4a8380b3ad
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-05-24 09:48:34 -0500

    Update Pipeline_SquamousCellCarcinoma_BipartiteAnalysis.R

--- Pipeline_SquamousCellCarcinoma_BipartiteAnalysis.R
+++ Pipeline_SquamousCellCarcinoma_BipartiteAnalysis.R
@@ -3,21 +3,29 @@ library(RCy3)
 library(RColorBrewer)
 # cyrestGET(operation = NULL, parameters = NULL, base.url = "http://localhost:1234")
 ############################################################################################################################################
-coexpression_network_Stage_I   <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_3_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/df_stageI_interactome.tsv"
-coexpression_network_Stage_II  <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_3_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/df_stageII_interactome.tsv"
-coexpression_network_Stage_III <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_3_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/df_stageIII_interactome.tsv"
-##################################################################################################
 interactome_network_Stage_I   <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_3_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/df_stageI_interactome_interactome.tsv"
 interactome_network_Stage_II  <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_3_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/df_stageII_interactome_interactome.tsv"
 interactome_network_Stage_III <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_3_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/df_stageIII_interactome_interactome.tsv"
 ##################################################################################################
-coexpression_network_Stage_I     <-read.table(file = coexpression_network_Stage_I, sep = '\t', header = TRUE,fill=TRUE)    #
-coexpression_network_Stage_II     <-read.table(file = coexpression_network_Stage_II, sep = '\t', header = TRUE,fill=TRUE)    #
-coexpression_network_Stage_III     <-read.table(file = coexpression_network_Stage_III, sep = '\t', header = TRUE,fill=TRUE)    #
-##################################################################################################
-interactome_network_Stage_I     <-read.table(file = interactome_network_Stage_I, sep = '\t', header = TRUE,fill=TRUE)    #
-interactome_network_Stage_II     <-read.table(file = interactome_network_Stage_II, sep = '\t', header = TRUE,fill=TRUE)    #
-interactome_network_Stage_III     <-read.table(file = interactome_network_Stage_III, sep = '\t', header = TRUE,fill=TRUE)    #
+interactome_network_Stage_I        <-read.table(file = interactome_network_Stage_I, sep = '\t', header = TRUE,fill=TRUE)    #
+interactome_network_Stage_II       <-read.table(file = interactome_network_Stage_II, sep = '\t', header = TRUE,fill=TRUE)    #
+interactome_network_Stage_III      <-read.table(file = interactome_network_Stage_III, sep = '\t', header = TRUE,fill=TRUE)    #
+#######################################################################################################################################
+file_unique_gene_stages_I    <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_3_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/DE_GenesPerStageMeansFromPairedUp_unique_stage_I.tsv"
+file_unique_gene_stages_II   <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_3_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/DE_GenesPerStageMeansFromPairedUp_unique_stage_II.tsv"
+file_unique_gene_stages_III  <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_3_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/DE_GenesPerStageMeansFromPairedUp_unique_stage_III.tsv"
+
+file_gene_stages_I    <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_3_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/DE_GenesPerStageMeansFromPairedUp_Stage_sample_stage_I.tsv"
+file_gene_stages_II   <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_3_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/DE_GenesPerStageMeansFromPairedUp_Stage_sample_stage_II.tsv"
+file_gene_stages_III  <-"/home/felipe/Documentos/LungPortal/output/threhold_RPKM_3_threhold_log2foldchange_1.0_FDR_0.05_threhold_correlation_0.99/DE_GenesPerStageMeansFromPairedUp_Stage_sample_stage_III.tsv"
+
+genes_Stage_I                <-read.table(file = file_gene_stages_I, sep = '\t', header = TRUE,fill=TRUE)    #
+genes_Stage_II               <-read.table(file = file_gene_stages_II, sep = '\t', header = TRUE,fill=TRUE)    #
+genes_Stage_III              <-read.table(file = file_gene_stages_III, sep = '\t', header = TRUE,fill=TRUE)    #
+
+genes_unique_Stage_I         <-read.table(file = file_unique_gene_stages_I, sep = '\t', header = TRUE,fill=TRUE)    #
+genes_unique_Stage_II        <-read.table(file = file_unique_gene_stages_II, sep = '\t', header = TRUE,fill=TRUE)    #
+genes_unique_Stage_III       <-read.table(file = file_unique_gene_stages_III, sep = '\t', header = TRUE,fill=TRUE)    #
 #######################################################################################################################################
 # Merge data.frame
 merge_interactome_gene_symbol <- merge_interactome_gene_symbol[match(unique(merge_interactome_gene_symbol$gene_id), merge_interactome_gene_symbol$gene_id),]
@@ -26,53 +34,80 @@ merge_interactome_gene_symbol <- merge_interactome_gene_symbol[match(unique(merg
 rownames(merge_interactome_gene_symbol)<-merge_interactome_gene_symbol$gene_id
 
 # Converted gene_ids
-coexpression_network_Stage_I    <-na.omit(data.frame(Gene1=merge_interactome_gene_symbol[coexpression_network_Stage_I$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[coexpression_network_Stage_I$Gene2,"gene_symbol"]))
-coexpression_network_Stage_II   <-na.omit(data.frame(Gene1=merge_interactome_gene_symbol[coexpression_network_Stage_II$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[coexpression_network_Stage_II$Gene2,"gene_symbol"]))
-coexpression_network_Stage_III  <-na.omit(data.frame(Gene1=merge_interactome_gene_symbol[coexpression_network_Stage_III$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[coexpression_network_Stage_III$Gene2,"gene_symbol"]))
+intractome_network_Stage_I    <-data.frame(Gene1=merge_interactome_gene_symbol[interactome_network_Stage_I$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[interactome_network_Stage_I$Gene2,"gene_symbol"])
+intractome_network_Stage_II   <-data.frame(Gene1=merge_interactome_gene_symbol[interactome_network_Stage_II$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[interactome_network_Stage_II$Gene2,"gene_symbol"])
+intractome_network_Stage_III  <-data.frame(Gene1=merge_interactome_gene_symbol[interactome_network_Stage_III$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[interactome_network_Stage_III$Gene2,"gene_symbol"])
 
 # Converted gene_ids
-intractome_network_Stage_I    <-na.omit(data.frame(Gene1=merge_interactome_gene_symbol[interactome_network_Stage_I$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[interactome_network_Stage_I$Gene2,"gene_symbol"]))
-intractome_network_Stage_II   <-na.omit(data.frame(Gene1=merge_interactome_gene_symbol[interactome_network_Stage_II$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[interactome_network_Stage_II$Gene2,"gene_symbol"]))
-intractome_network_Stage_III  <-na.omit(data.frame(Gene1=merge_interactome_gene_symbol[interactome_network_Stage_III$Gene1,"gene_symbol"],Gene2=merge_interactome_gene_symbol[interactome_network_Stage_III$Gene2,"gene_symbol"]))
+symbols_unique_Stage_I        <-merge_interactome_gene_symbol[genes_unique_Stage_I$gene,"gene_symbol"]
+symbols_unique_Stage_II       <-merge_interactome_gene_symbol[genes_unique_Stage_II$gene,"gene_symbol"]
+symbols_unique_Stage_III      <-merge_interactome_gene_symbol[genes_unique_Stage_III$gene,"gene_symbol"]
+
+# Converted gene_ids
+symbols_Stage_I        <-merge_interactome_gene_symbol[genes_Stage_I$gene,"gene_symbol"]
+symbols_Stage_II       <-merge_interactome_gene_symbol[genes_Stage_II$gene,"gene_symbol"]
+symbols_Stage_III      <-merge_interactome_gene_symbol[genes_Stage_III$gene,"gene_symbol"]
 #######################################################################################################################################
 # Read igraph
 interactome_network_Stage_I<-graph_from_data_frame(interactome_network_Stage_I, directed = FALSE, vertices = NULL)
 interactome_network_Stage_II<-graph_from_data_frame(interactome_network_Stage_II, directed = FALSE, vertices = NULL)
 interactome_network_Stage_III<-graph_from_data_frame(interactome_network_Stage_III, directed = FALSE, vertices = NULL)
 ############################################################################################################################################
-# greedy method (hiearchical, fast method)
-cluster_interactome_stage_I  <- cluster_fast_greedy(interactome_network_Stage_I)
-cluster_interactome_stage_II <- cluster_fast_greedy(interactome_network_Stage_II)
-cluster_interactome_stage_III <- cluster_fast_greedy(interactome_network_Stage_III)
-############################################################################################################################################
-############################################################################################################################################
+# Store genes stage I, II and III
+# Vectors to store gene ids from each stage
+genes_id_vector_stage_I<-c()
+genes_id_vector_stage_II<-c()
+genes_id_vector_stage_III<-c()
+
+# For each gene in stage I
+for (gene_id in genes_Stage_I$gene)
+{
+  # Store gene id in the vector
+  genes_id_vector_stage_I<-c(genes_id_vector_stage_I,strsplit(gene_id, split = "\\.")[[1]][1])
+}
+# For each gene in stage II
+for (gene_id in genes_Stage_II$gene)
+{
+  # Store gene id in the vector
+  genes_id_vector_stage_II<-c(genes_id_vector_stage_II,strsplit(gene_id, split = "\\.")[[1]][1])
+}
+# For each gene in stage III
+for (gene_id in genes_Stage_III$gene)
+{
+  # Store gene id in the vector
+  genes_id_vector_stage_III<-c(genes_id_vector_stage_III,strsplit(gene_id, split = "\\.")[[1]][1])
+}
+# 
+#genes_id_vector_stage_I   <-merge_interactome_gene_symbol[genes_id_vector_stage_I,"gene_symbol"]
+#genes_id_vector_stage_II  <-merge_interactome_gene_symbol[genes_id_vector_stage_II,"gene_symbol"]
+#genes_id_vector_stage_III <-merge_interactome_gene_symbol[genes_id_vector_stage_III,"gene_symbol"]
+########################################################################################################################################
 # Set colour
 nb.cols  <- max(c(length(unique(membership(cluster_interactome_stage_I))),length(unique(membership(cluster_interactome_stage_II))), length(unique(membership(cluster_interactome_stage_III)))))
 mycolors <- data.frame(colour=colorRampPalette(brewer.pal(8, "Set3"))(nb.cols))
-
-# Generate colors based on media type:
-V(interactome_network_Stage_I)$color  <- mycolors[as.numeric( membership(cluster_interactome_stage_I)),]
-V(interactome_network_Stage_II)$color <- mycolors[as.numeric( membership(cluster_interactome_stage_II)),]
-V(interactome_network_Stage_III)$color<- mycolors[as.numeric( membership(cluster_interactome_stage_III)),]
 
 # Set node size based on audience size:
 V(interactome_network_Stage_I)$size <- log(degree(interactome_network_Stage_I)+0.0001,2)*2
 V(interactome_network_Stage_II)$size <- log(degree(interactome_network_Stage_II)+0.0001,2)*2
 V(interactome_network_Stage_III)$size <- log(degree(interactome_network_Stage_III)+0.0001,2)*2
 
+sum(rownames(data.frame(V(interactome_network_Stage_I)))  %in% genes_id_vector_stage_I)
+sum(rownames(data.frame(V(interactome_network_Stage_II)))  %in% genes_id_vector_stage_II)
+sum(rownames(data.frame(V(interactome_network_Stage_III)))  %in% genes_id_vector_stage_III)
+########################################################################################################################################
 # FindClusters_resolution                                                                                                                                                                                                   #
 png(filename=paste(output_dir,"Panel_subgraph_interactome_stage_I.png",sep=""), width = 30, height = 30, res=600, units = "cm")                                                                                                    #    
-  plot(interactome_network_Stage_I,layout=    layout_with_mds, edge.color	="black", vertex.label=NA)
+  plot(interactome_network_Stage_I,layout=    layout_with_mds, edge.color	="black", vertex.color	="grey", vertex.label=NA)
 dev.off() 
 
 # FindClusters_resolution                                                                                                                                                                                                   #
 png(filename=paste(output_dir,"Panel_subgraph_interactome_stage_II.png",sep=""), width = 30, height = 30, res=600, units = "cm")                                                                                                    #    
-  plot(interactome_network_Stage_II,layout=    layout_with_mds, edge.color	="black", vertex.label=NA)
+  plot(interactome_network_Stage_I,layout=    layout_with_mds, edge.color	="black", vertex.color	="grey", vertex.label=NA)
 dev.off() 
 
 # FindClusters_resolution                                                                                                                                                                                                   #
 png(filename=paste(output_dir,"Panel_subgraph_interactome_stage_III.png",sep=""), width = 30, height = 30, res=600, units = "cm")                                                                                                    #    
-  plot(interactome_network_Stage_III,layout=    layout_with_mds, edge.color	="black", vertex.label=NA)
+  plot(interactome_network_Stage_I,layout=    layout_with_mds, edge.color	="black", vertex.color	="grey", vertex.label=NA)
 dev.off() 
 
 

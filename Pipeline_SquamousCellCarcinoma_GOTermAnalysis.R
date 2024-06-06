06e1ab9c464d6b7a1be83106549159ceb1edc500
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-06-06 11:38:02 -0500

    Update Pipeline_SquamousCellCarcinoma_GOTermAnalysis.R

--- Pipeline_SquamousCellCarcinoma_GOTermAnalysis.R
+++ Pipeline_SquamousCellCarcinoma_GOTermAnalysis.R
@@ -49,48 +49,44 @@ ids_stage_I    <-bitr(genes_unique_Stage_I$gene_id, fromType = "ENSEMBL", toType
 ids_stage_II   <-bitr(genes_unique_Stage_II$gene_id, fromType = "ENSEMBL", toType = c("ENTREZID","SYMBOL"), OrgDb="org.Hs.eg.db")
 ids_stage_III  <-bitr(genes_unique_Stage_III$gene_id, fromType = "ENSEMBL", toType = c("ENTREZID","SYMBOL"), OrgDb="org.Hs.eg.db")
 ########################################################################################################################################
-colnames(ids_stage_I)   <-c("gene_id","ENTREZID")
-colnames(ids_stage_II)  <-c("gene_id","ENTREZID")
-colnames(ids_stage_III) <-c("gene_id","ENTREZID")
+colnames(ids_stage_I)   <-c("gene_id","ENTREZID","SYMBOL")
+colnames(ids_stage_II)  <-c("gene_id","ENTREZID","SYMBOL")
+colnames(ids_stage_III) <-c("gene_id","ENTREZID","SYMBOL")
 ########################################################################################################################################
 genes_Stage_I  <-merge(genes_unique_Stage_I,ids_stage_I,by="gene_id")
 genes_Stage_II <-merge(genes_unique_Stage_II,ids_stage_II,by="gene_id")
 genes_Stage_III<-merge(genes_unique_Stage_III,ids_stage_III,by="gene_id")
-genes_Stage_ALL<-rbind(genes_Stage_I,genes_Stage_II,genes_Stage_III)
+genes_Stage_ALL<-unique(rbind(ids_stage_I,ids_stage_II,ids_stage_III))
+rownames(genes_Stage_ALL)<-genes_Stage_ALL$ENTREZID
 ########################################################################################################################################
 go_ALL_Stage_I    <- enrichGO(gene = ids_stage_I$SYMBOL,  OrgDb  = org.Hs.eg.db,      ont = "ALL", pAdjustMethod = "BH",pvalueCutoff  = 0.05,qvalueCutoff  = 0.05,readable = TRUE, minGSSize = 3,keyType = "SYMBOL")
-go_ALL_Stage_II   <- enrichGO(gene = ids_stage_II$ENTREZ, OrgDb  = org.Hs.eg.db,      ont = "ALL",  pAdjustMethod = "BH",pvalueCutoff  = 0.05,qvalueCutoff  = 0.05,readable = TRUE, minGSSize = 3,keyType = "SYMBOL")
-go_ALL_Stage_III  <- enrichGO(gene = ids_stage_III$ENTREZ,OrgDb  = org.Hs.eg.db,      ont = "ALL",   pAdjustMethod = "BH",pvalueCutoff  = 0.05,qvalueCutoff  = 0.05,readable = TRUE, minGSSize = 3,keyType = "SYMBOL")
-go_ALL_Stage      <- enrichGO(gene = genes_Stage_ALL$ENTREZ,OrgDb  = org.Hs.eg.db,    ont = "ALL",   pAdjustMethod = "BH",pvalueCutoff  = 0.05,qvalueCutoff  = 0.05,readable = TRUE, minGSSize = 3,keyType = "SYMBOL")
+go_ALL_Stage_II   <- enrichGO(gene = ids_stage_II$SYMBOL, OrgDb  = org.Hs.eg.db,      ont = "ALL",  pAdjustMethod = "BH",pvalueCutoff  = 0.05,qvalueCutoff  = 0.05,readable = TRUE, minGSSize = 3,keyType = "SYMBOL")
+go_ALL_Stage_III  <- enrichGO(gene = ids_stage_III$SYMBOL,OrgDb  = org.Hs.eg.db,      ont = "ALL",   pAdjustMethod = "BH",pvalueCutoff  = 0.05,qvalueCutoff  = 0.05,readable = TRUE, minGSSize = 3,keyType = "SYMBOL")
+go_ALL_Stage      <- enrichGO(gene = genes_Stage_ALL$SYMBOL,OrgDb  = org.Hs.eg.db,    ont = "ALL",   pAdjustMethod = "BH",pvalueCutoff  = 0.05,qvalueCutoff  = 0.05,readable = TRUE, minGSSize = 3,keyType = "SYMBOL")
 ########################################################################################################################################
-kegg_ALL_Stage_I    <- enrichKEGG(gene = ids_stage_I$SYMBOL,  organism     = 'hsa',    pvalueCutoff = 0.15,keyType = "SYMBOL")
-kegg_ALL_Stage_II   <- enrichKEGG(gene = ids_stage_II$ENTREZ,  organism     = 'hsa',    pvalueCutoff = 0.15,keyType = "SYMBOL")
-kegg_ALL_Stage_III  <- enrichKEGG(gene = ids_stage_III$ENTREZ,  organism     = 'hsa',    pvalueCutoff = 0.15,keyType = "SYMBOL")
-kegg_ALL_Stage      <- enrichKEGG(gene = genes_Stage_ALL$ENTREZ,  organism     = 'hsa',    pvalueCutoff = 0.15,keyType = "SYMBOL")
+kegg_ALL_Stage_I    <- enrichKEGG(gene = ids_stage_I$ENTREZ,  organism     = 'hsa',    pvalueCutoff = 0.15)
+kegg_ALL_Stage_II   <- enrichKEGG(gene = ids_stage_II$ENTREZ,  organism     = 'hsa',    pvalueCutoff = 0.15)
+kegg_ALL_Stage_III  <- enrichKEGG(gene = ids_stage_III$ENTREZ,  organism     = 'hsa',    pvalueCutoff = 0.15)
+kegg_ALL_Stage      <- enrichKEGG(gene = genes_Stage_ALL$ENTREZ,  organism     = 'hsa',    pvalueCutoff = 0.15)
 ########################################################################################################################################
-# convert ids
-genes_Stage_ALL[genes_Stage_ALL$ENTREZID %in% kegg_ALL_Stage_I@result$geneID ,]
-
-
-
+reactome_ALL_Stage_I <- enrichPathway(gene=ids_stage_I$ENTREZ, pvalueCutoff = 0.05, readable=TRUE)
+reactome_ALL_Stage_II <- enrichPathway(gene=ids_stage_II$ENTREZ, pvalueCutoff = 0.05, readable=TRUE)
+reactome_ALL_Stage_III <- enrichPathway(gene=ids_stage_III$ENTREZ, pvalueCutoff = 0.05, readable=TRUE)
+########################################################################################################################################
+# Convert symbols
+#kegg_ALL_Stage_I@result$geneID<-genes_Stage_ALL[kegg_ALL_Stage_I@result$geneID ,"SYMBOL"]
+#kegg_ALL_Stage_II@result$geneID<-genes_Stage_ALL[kegg_ALL_Stage_II@result$geneID ,"SYMBOL"]
+#kegg_ALL_Stage_III@result$geneID<-genes_Stage_ALL[kegg_ALL_Stage_III@result$geneID ,"SYMBOL"]
+#kegg_ALL_Stage@result$geneID<-genes_Stage_ALL[kegg_ALL_Stage@result$geneID ,"SYMBOL"]
+########################################################################################################################################
 # FindClusters_resolution
 png(filename=paste(output_folder,"Plot_cnept_plot_Stage_I.png",sep=""), width = 20, height = 20, res=600, units = "cm")
-	cnetplot(gse_ALL_Stage_I, showCategory = 10, layout = "kk") + ggtitle("Stage I") 
+	cnetplot(kegg_ALL_Stage, showCategory = 3, layout = "kk") + ggtitle("KEGG pathway - all stages") 
 dev.off()
 # FindClusters_resolution
 png(filename=paste(output_folder,"Plot_cnept_plot_Stage_II.png",sep=""), width = 20, height = 20, res=600, units = "cm")
-	cnetplot(gse_ALL_Stage_II, showCategory = 5, layout = "kk") + ggtitle("Stage II") 
+	cnetplot(go_ALL_Stage, showCategory = 3, layout = "kk") + ggtitle("GO Terms - all stages") 
 dev.off()
-# FindClusters_resolution
-png(filename=paste(output_folder,"Plot_cnept_plot_Stage_III.png",sep=""), width = 20, height = 20, res=600, units = "cm")
-	cnetplot(gse_ALL_Stage_III, showCategory = 5, layout = "kk") + ggtitle("Stage III") 
-dev.off()
-
-# FindClusters_resolution
-png(filename=paste(output_folder,"Plot_cnept_genes_Stage_ALL.png",sep=""), width = 30, height = 30, res=600, units = "cm")
-	cnetplot(genes_Stage_ALL, showCategory = 10, layout = "kk",color_gene = "black") + ggtitle("All stages") 
-dev.off()
-
 ########################################################################################################################################
 write.xlsx(x=genes_Stage_I,file=paste(output_dir,"unique_genes",".xlsx",sep=""), sheet="Stage I")
 write.xlsx(x=genes_Stage_II,file=paste(output_dir,"unique_genes",".xlsx",sep=""), sheet="Stage II")

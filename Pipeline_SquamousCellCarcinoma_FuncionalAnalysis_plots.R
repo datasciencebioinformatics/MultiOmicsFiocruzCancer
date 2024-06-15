35de9c4110e9bd77a17c9702c627fcd5bfae25eb
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-06-15 13:10:53 -0500

    Update Pipeline_SquamousCellCarcinoma_FuncionalAnalysis_plots.R

--- Pipeline_SquamousCellCarcinoma_FuncionalAnalysis_plots.R
+++ Pipeline_SquamousCellCarcinoma_FuncionalAnalysis_plots.R
@@ -29,16 +29,18 @@ for (annotation in rownames(annotation_stages_all))
 	GeneCards_Summary    <- annotation_stages_all[annotation,"GeneCards_Summary"]   
 	Stage                <- annotation_stages_all[annotation,"Stage"]   
 	CluterProfiler	     <- annotation_stages_all[annotation,"CluterProfiler"]   
+	GeneCards	     <- annotation_stages_all[annotation,"GeneCards"]
 	Layer       	     <- ""
 
 	# All genes of stage
-	all_genes_of_annotation<-strsplit(x=CluterProfiler,",",fixed=T)[[1]]
-
+	all_CluterProfiler<-strsplit(x=CluterProfiler,",",fixed=T)[[1]]
+	all_GeneCards<-strsplit(x=GeneCards,",",fixed=T)[[1]]
+	
 	# For all genes
-	for (CluterProfiler_index in all_genes_of_annotation)
+	for (CluterProfiler_index in all_CluterProfiler)
 	{
 		# Set layer
-		Layer<-strsplit(x=CluterProfiler_index,":",fixed=T)[[1]][[1]]
+		Layer<-strsplit(x=all_CluterProfiler,":",fixed=T)[[1]][[1]]
 		
 		# If layer equal to Kegg
 		if(Layer=="KEGG")
@@ -53,13 +55,36 @@ for (annotation in rownames(annotation_stages_all))
 			genes_id<-genes_Stage_ALL[which(genes_Stage_ALL$SYMBOL %in% genes),"gene_id"]
 		}
 		# gene annotation
-		gene_annotation<-data.frame(gene_id=gene_id,gene=gene,log2change=log2change,Category=Category,Pvalue=Pvalue,FDR=FDR,ENTREZID=ENTREZID,Symbol=Symbol,Description=Description,genecards_Category=genecards_Category,UniProt_ID=UniProt_ID,GIFtS=GIFtS,GC_id=GC_id,GeneCards_Summary=GeneCards_Summary,Stage=Stage,Layer=Layer,CluterProfiler=CluterProfiler_index )
+		gene_annotation<-data.frame(gene_id=gene_id,gene=gene,log2change=log2change,Category=Category,Pvalue=Pvalue,FDR=FDR,ENTREZID=ENTREZID,Symbol=Symbol,Description=Description,genecards_Category=genecards_Category,UniProt_ID=UniProt_ID,GIFtS=GIFtS,GC_id=GC_id,GeneCards_Summary=GeneCards_Summary,Stage=Stage,Layer=Layer,CluterProfiler=str_trim(CluterProfiler_index, side="left") )
 
 		# df_all_annotation
 		df_all_annotation<-rbind(df_all_annotation,gene_annotation)
 	}	
+	# For all genes
+	for (Genecards_index in all_GeneCards)
+	{
+		# Set layer
+		Layer<-strsplit(x=all_CluterProfiler,":",fixed=T)[[1]][[1]]
+		
+		# If layer equal to Kegg
+		if(Layer=="KEGG")
+		{
+			# Make the id converstion			
+			gene_symbol<-genes_Stage_ALL[which(genes_Stage_ALL$ENTREZID %in% genes),"SYMBOL"]
+			genes_id<-genes_Stage_ALL[which(genes_Stage_ALL$ENTREZID %in% genes),"gene_id"]
+		}else # If not kegg 
+		{
+			# Make the id converstion			
+			gene_symbol<-genes_Stage_ALL[which(genes_Stage_ALL$SYMBOL %in% genes),"SYMBOL"]
+			genes_id<-genes_Stage_ALL[which(genes_Stage_ALL$SYMBOL %in% genes),"gene_id"]
+		}
+		# gene annotation
+		gene_annotation<-data.frame(gene_id=gene_id,gene=gene,log2change=log2change,Category=Category,Pvalue=Pvalue,FDR=FDR,ENTREZID=ENTREZID,Symbol=Symbol,Description=Description,genecards_Category=genecards_Category,UniProt_ID=UniProt_ID,GIFtS=GIFtS,GC_id=GC_id,GeneCards_Summary=GeneCards_Summary,Stage=Stage,Layer=Layer,CluterProfiler=str_trim(Genecards_index, side="left") )
+	
+		# df_all_annotation
+		df_all_annotation<-rbind(df_all_annotation,gene_annotation)
+	}		
 }
-
 
 
 
@@ -90,103 +115,6 @@ interactome_all_stage$Gene2<-genes_in_interactome[interactome_all_stage$Gene2,"S
 
 # gene annotation
 interactome_annotation<-data.frame(gene_id=rownames(interactome_all_stage),gene=rownames(interactome_all_stage),log2change=rownames(interactome_all_stage),Category=0,Pvalue=0,FDR=0,ENTREZID=0,Symbol=interactome_all_stage$Gene1,Description=0,genecards_Category=0,UniProt_ID=0,GIFtS=0,GC_id=0,GeneCards_Summary=0,Stage=interactome_all_stage$Stage,Layer="Interactome",CluterProfiler=interactome_all_stage$Gene2 )
-######################################################################################################################
-# Store annotation of stages
-df_all_annotation_per_stage<-df_all_annotation
-
-# interactome_annotation
-interactome_annotation_stage<-interactome_annotation
-
-# interactome_annotation_stage
-selected_interactome<-c(paste(interactome_annotation_stage$Description,interactome_annotation_stage$SYMBOL,sep="-"),
-paste(interactome_annotation_stage$SYMBOL,interactome_annotation_stage$Description,sep="-"))
-######################################################################################################################
-# A table for the count of go terms
-table_GO<-table(df_all_annotation_per_stage[df_all_annotation_per_stage$Layer=="GO","CluterProfiler"])
-table_KEGG<-table(df_all_annotation_per_stage[df_all_annotation_per_stage$Layer=="KEGG","CluterProfiler"])
-table_REACTOME<-table(df_all_annotation_per_stage[df_all_annotation_per_stage$Layer=="Reactome","CluterProfiler"])
-
-all_selection<-c(table_GO,table_KEGG,table_REACTOME)
-
-selection_all         <-names(tail(sort(all_selection),n=10))
-
-# Count per selected term
-data.frame(n=tail(sort(all_selection),n=10))
-
-df_all_annotation_selected_pathways<-rbind(df_all_annotation_per_stage[which(df_all_annotation_per_stage$CluterProfiler %in% selection_all),],
-interactome_annotation_stage)
-####################################################################################################################
-# All stages
-graph_all_stages <- graph_from_data_frame(d=unique(df_all_annotation_selected_pathways[,c("Symbol","CluterProfiler")]), vertices=unique(c(df_all_annotation_selected_pathways$Symbol,df_all_annotation_selected_pathways$CluterProfiler)), directed=F)  
-
-# df_all_eges
-df_all_eges<-data.frame(get.edgelist(graph_all_stages))
-
-# Set ronames
-df_all_eges$names<-paste(df_all_eges$X1,df_all_eges$X2,sep="-")
-####################################################################################################################
-# Vertice colours of genes
-V(graph_all_stages)$color                                                                                                  <- "black"
-V(graph_all_stages)[which(names(V(graph_all_stages)) %in% df_all_annotation_selected_pathways$CluterProfiler)]$color       <- "black"
-
-V(graph_all_stages)[which(names(V(graph_all_stages)) %in% ids_stage_I$SYMBOL)]$color       <- "#E69F00"
-V(graph_all_stages)[which(names(V(graph_all_stages)) %in% ids_stage_II$SYMBOL)]$color      <- "#009E73"
-V(graph_all_stages)[which(names(V(graph_all_stages)) %in% ids_stage_III$SYMBOL)]$color     <- "#D81B60"
-
-# Vertice colours of genes
-V(graph_all_stages)$shape                                                                                                                                                                                      <-"circle"
-V(graph_all_stages)[which(names(V(graph_all_stages)) %in%  df_all_annotation_selected_pathways[df_all_annotation_selected_pathways$Layer %in% c("GO","KEGG","Reactome"),"CluterProfiler"])]$shape              <- "square"
-
-# Set size of the node according to the dregree
-V(graph_all_stages)$size                                                                                                                                                                               <- 5
-V(graph_all_stages)[which(names(V(graph_all_stages)) %in% df_all_annotation_selected_pathways[df_all_annotation_selected_pathways$Layer %in% c("GO","KEGG","Reactome"),"CluterProfiler"])]$size        <- 7
-
-# Vertice colours of genes
-E(graph_all_stages)$color                                                                         <- "black"
-
-# Set ronames
-E(graph_all_stages)[which(df_all_eges$names %in% selected_interactome)]$color                     <- "lightgrey"
-
-# FindClusters_resolution
-png(filename=paste(output_folder,"Plot_Stage_all.png",sep=""), width = 30, height = 30, res=600, units = "cm")
-	#plot(graph_all_stages, layout=layout_nicely, vertex.label=NA) # Stage I
-	#plot(graph_all_stages, layout=  layout_with_kk, vertex.label=NA) # Stage II
-	plot(graph_all_stages, layout=   layout_nicely, vertex.label=NA) # Stage II
-dev.off()
-
-# FindClusters_resolution
-png(filename=paste(output_folder,"Plot_Stage_label.png",sep=""), width = 30, height = 30, res=600, units = "cm")
-	#plot(graph_all_stages, layout=layout_nicely) # Stage I
-	#plot(graph_all_stages, layout=  layout_with_kk) # Stage II
-	plot(graph_all_stages, layout=   layout_nicely) # Stage II
-dev.off()
-
-
-# Save file 
-write.xlsx(x=df_all_annotation_selected_pathways,file=paste(output_dir,"unique_genes_annotation_clusterProfiler",".xlsx",sep=""), sheet="ten most abundant annotation", append=TRUE)
-
-# Set legend
-png(filename=paste(output_folder,"legend.png",sep=""), width = 5, height = 5, res=600, units = "cm")
-plot(NULL ,xaxt='n',yaxt='n',bty='n',ylab='',xlab='', xlim=0:1, ylim=0:1)
-legend("topleft", legend =c('GO', 'KEGG', 'REACTOME','Gene'), pch=16, pt.cex=3, cex=1.5, bty='n',col = c('#ff6347', '#ffd700', '#7f7f7f', '#0072b2'))
-mtext("Legend", at=0.2, cex=2)
-dev.off()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ######################################################################################################################
 # Next,  ten most abundant annotation terms in number of genes per stage additionally inspected.
 ######################################################################################################################
@@ -326,7 +254,7 @@ E(graph_all_stages)$color                                                       
 E(graph_all_stages)[which(df_all_eges$names %in% selected_interactome)]$color                     <- "lightgrey"
 
 # Vertice colours of genes
-V(graph_all_stages)$labels                                                                        <- ""                                                                                                                                                                                   <-"circle"
+#V(graph_all_stages)$labels                                                                        <- ""                                                                                                                                                                                   <-"circle"
 
 
 terms_stage_III<-names(which(!boolean_counts_terms[,c("Stage_I")] & !boolean_counts_terms[,c("Stage_II")] & boolean_counts_terms[,c("Stage_III")]))
@@ -339,7 +267,8 @@ V(graph_all_stages)[which(names(V(graph_all_stages)) %in% genes_stage_III)]$labe
 png(filename=paste(output_folder,"Plot_Stage_all_per_Stagge.png",sep=""), width = 30, height = 30, res=600, units = "cm")
 	#plot(graph_all_stages, layout=layout_nicely, vertex.label=NA) # Stage I
 	#plot(graph_all_stages, layout=  layout_with_kk, vertex.label=NA) # Stage II
-	plot(graph_all_stages, layout=   layout_nicely, vertex.label=V(graph_all_stages)$labels) # Stage II
+	#plot(graph_all_stages, layout=   layout_nicely, vertex.label=V(graph_all_stages)$labels) # Stage II
+	plot(graph_all_stages, layout=   layout_nicely,vertex.label=NA ) # Stage II
 dev.off()
 
 plot(graph_all_stages, layout=   layout_nicely, vertex.label=NA) # Stage II

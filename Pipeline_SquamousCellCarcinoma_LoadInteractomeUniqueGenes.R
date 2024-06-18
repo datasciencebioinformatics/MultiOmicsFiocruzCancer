850f88104e59c654a519ad333d7ac2ce1b9518cb
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-06-18 08:31:23 -0500

    Update Pipeline_SquamousCellCarcinoma_LoadInteractomeUniqueGenes.R

--- Pipeline_SquamousCellCarcinoma_LoadInteractomeUniqueGenes.R
+++ Pipeline_SquamousCellCarcinoma_LoadInteractomeUniqueGenes.R
@@ -246,6 +246,8 @@ for (edge_i in rownames(net_stage_all_correlation_network))
   }
 }
 net_stage_all_correlation_network<-na.omit(unique(net_stage_all_correlation_network[selected_edges,]))
+
+net_stage_all_correlation_network$Gene1 %in% 
 #######################################################################################################
 # Load interactome
 genes_in_interactome<-unique(genes_Stage_ALL[genes_Stage_ALL$gene_id %in% intersect(genes_Stage_ALL$gene_id,c(interactome_all_stage$Gene1,interactome_all_stage$Gene2,net_stage_all_correlation_network$Gene1,net_stage_all_correlation_network$Gene2)),])

f592386ca29dc876e63a47d8f958e6e3740993b5
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-06-20 13:47:03 -0500

    Update Pipeline_SquamousCellCarcinoma_ExpressionPlots.R

--- Pipeline_SquamousCellCarcinoma_ExpressionPlots.R
+++ Pipeline_SquamousCellCarcinoma_ExpressionPlots.R
@@ -1,8 +1,3 @@
-# Take genes per each stage
-genes_stage_I<-gsub(" ", "", unique(unique(strsplit(x=paste(unique(df_count_terms_selected$Genes_Stage_I),collapse=","),split=",",fixed=T))[[1]]))
-genes_stage_II<-gsub(" ", "", unique(unique(strsplit(x=paste(unique(df_count_terms_selected$Genes_Stage_II),collapse=","),split=",",fixed=T))[[1]]))
-genes_stage_III<-gsub(" ", "", unique(unique(strsplit(x=paste(unique(df_count_terms_selected$Genes_Stage_III),collapse=","),split=",",fixed=T))[[1]]))
-
 ##################################################################################################################################################
 # Remove empty line
 genes_stage_I<-genes_stage_I[genes_stage_I!=""]
@@ -29,17 +24,30 @@ sample_stage_III_normal  <-samples_normal[samples_normal$stages=="Stage III",c("
 # Select samples                                                                                                                                                          #
 sample_stage_all_samples<-rbind(sample_stage_I_tumor,sample_stage_I_normal,sample_stage_II_tumor,sample_stage_II_normal,sample_stage_III_tumor,sample_stage_III_normal)   #
 ############################################################################################################################################################################
+# Take genes per each stage
+genes_stage_I<-gsub(" ", "", unique(unique(strsplit(x=paste(unique(df_count_terms_selected$Genes_Stage_I),collapse=","),split=",",fixed=T))[[1]]))
+genes_stage_II<-gsub(" ", "", unique(unique(strsplit(x=paste(unique(df_count_terms_selected$Genes_Stage_II),collapse=","),split=",",fixed=T))[[1]]))
+genes_stage_III<-gsub(" ", "", unique(unique(strsplit(x=paste(unique(df_count_terms_selected$Genes_Stage_III),collapse=","),split=",",fixed=T))[[1]]))
+genes_stage_I<-genes_stage_I[genes_stage_I!=""]
+genes_stage_II<-genes_stage_II[genes_stage_II!=""]
+genes_stage_III<-genes_stage_III[genes_stage_III!=""]
+############################################################################################################################################################################
 # Samples
 unstranded_data_samples<-melt(t(unstranded_data_filter[,sample_stage_all_samples$patient_id]))
+unstranded_data_samples_unapaired<-melt(t(unstranded_data_filter[,colDta_tumor$patient_id]))
 
 # colnames(unstranded_data_samples)
 colnames(unstranded_data_samples)<-c("patient_id","gene_id","RPKM")
+colnames(unstranded_data_samples_unapaired)<-c("patient_id","gene_id","RPKM")
+
 
 # unstranded_data_samples with sample_stage_all_samples
 unstranded_data_samples<-merge(unstranded_data_samples,sample_stage_all_samples,by="patient_id")
+unstranded_data_samples_unapaired<-merge(unstranded_data_samples_unapaired,sample_stage_all_samples,by="patient_id")
 
 # colnames
 colnames(unstranded_data_samples)<-c("patient_id","gene_id","RPKM","tissue_type","stages")
+colnames(unstranded_data_samples_unapaired)<-c("patient_id","gene_id","RPKM","tissue_type","stages")
 
 # genes_ids
 df_id_conversion<-data.frame(Gene=c(),Gene_id=c())
@@ -60,34 +68,64 @@ selected_ids<-df_id_conversion[df_id_conversion$Gene %in% c(genes_unique_Stage_I
 
 # Take only selected entries
 unstranded_data_samples<-unstranded_data_samples[which(unstranded_data_samples$gene_id %in% selected_ids$Gene_id),]
+unstranded_data_samples_unapaired<-unstranded_data_samples_unapaired[which(unstranded_data_samples_unapaired$gene_id %in% selected_ids$Gene_id),]
 
 # Set colnames
 colnames(df_id_conversion)[1]<-c("gene_id")
 
 # unstranded_data_samples
 unstranded_data_samples<-merge(unstranded_data_samples,df_id_conversion,by="gene_id")
+unstranded_data_samples_unapaired<-merge(unstranded_data_samples_unapaired,df_id_conversion,by="gene_id")
 ############################################################################################################################################################################
 my_comparisons <- list( c("Stage I", "Stage II"), c("Stage I", "Stage III"), c("Stage II", "Stage III") )
 ############################################################################################################################################################################
 # ids_stage_I - all ENSEMBL anotated using bitr
 ids_translation      <-bitr(unstranded_data_samples$Gene, fromType = "ENSEMBL", toType = c("ENTREZID","SYMBOL","ENSEMBL"), OrgDb="org.Hs.eg.db")
+ids_translation_unpaired      <-bitr(unstranded_data_samples_unapaired$Gene, fromType = "ENSEMBL", toType = c("ENTREZID","SYMBOL","ENSEMBL"), OrgDb="org.Hs.eg.db")
 
 # unstranded_data_samples$Gene
 unstranded_data_samples$ENSEMBL<-unstranded_data_samples$Gene
+unstranded_data_samples_unapaired$ENSEMBL<-unstranded_data_samples_unapaired$Gene
 
 # Merge symbols
 unstranded_data_samples<-merge(unstranded_data_samples,ids_translation,by="ENSEMBL")
+unstranded_data_samples_unapaired<-merge(unstranded_data_samples_unapaired,ids_translation,by="ENSEMBL")
+###############################################################################################################
+# Stage I
+stage_I_selected_genes<-c("KPNA4","DHX36","CXADR")
 
+# Stage II
+stage_II_selected_genes<-c("QTRT1","MYBBP1A","THY1")
+
+# Stage III
+stage_III_selected_genes<-c("BRMS1","CPSF1","IDH1")
+###############################################################################################################
+# change box plot line colors by groups
+p_stage_I_paired<-ggplot(unstranded_data_samples[unstranded_data_samples$SYMBOL %in% stage_I_selected_genes,], aes(x=stages, y=RPKM, fill=tissue_type)) +   geom_boxplot()+ facet_wrap(~SYMBOL, ncol = 3, scales="free")+ theme_bw() + ggtitle("Stage I genes. Paired samples")
+p_stage_I_unpaired<-ggplot(unstranded_data_samples_unapaired[unstranded_data_samples_unapaired$SYMBOL %in% stage_I_selected_genes,], aes(x=stages, y=RPKM)) +   geom_boxplot()+ facet_wrap(~SYMBOL, ncol = 3, scales="free")+ theme_bw()  + stat_compare_means(comparisons = my_comparisons, method="t.test") + ggtitle("Stage I genes. Tumor samples")
 
 # change box plot line colors by groups
-p<-ggplot(unstranded_data_samples[unstranded_data_samples$Gene %in% genes_unique_Stage_I$gene_id,], aes(x=stages, y=RPKM, fill=tissue_type)) +   geom_boxplot()+ facet_wrap(~SYMBOL, ncol = 5, scales="free")+ theme_bw() 
+p_stage_II_paired<-ggplot(unstranded_data_samples[unstranded_data_samples$SYMBOL %in% stage_II_selected_genes,], aes(x=stages, y=RPKM, fill=tissue_type)) +   geom_boxplot()+ facet_wrap(~SYMBOL, ncol = 3, scales="free")+ theme_bw() + ggtitle("Stage II genes. Paired samples")
+p_stage_II_unpaired<-ggplot(unstranded_data_samples_unapaired[unstranded_data_samples_unapaired$SYMBOL %in% stage_II_selected_genes,], aes(x=stages, y=RPKM)) +   geom_boxplot()+ facet_wrap(~SYMBOL, ncol = 3, scales="free")+ theme_bw()  + stat_compare_means(comparisons = my_comparisons, method="t.test") + ggtitle("Stage II genes. Tumor samples")
+
+# change box plot line colors by groups
+p_stage_III_paired<-ggplot(unstranded_data_samples[unstranded_data_samples$SYMBOL %in% stage_III_selected_genes,], aes(x=stages, y=RPKM, fill=tissue_type)) +   geom_boxplot()+ facet_wrap(~SYMBOL, ncol = 3, scales="free")+ theme_bw() + ggtitle("Stage III genes. Paired samples")
+p_stage_III_unpaired<-ggplot(unstranded_data_samples_unapaired[unstranded_data_samples_unapaired$SYMBOL %in% stage_III_selected_genes,], aes(x=stages, y=RPKM)) +   geom_boxplot()+ facet_wrap(~SYMBOL, ncol = 3, scales="free")+ theme_bw()  + stat_compare_means(comparisons = my_comparisons, method="t.test") + ggtitle("Stage III genes. Tumor samples")
+############################################################################################################################################################################
+# FindClusters_resolution
+png(filename=paste(output_folder,"Plot_p_stage_I_paired.png",sep=""), width = 30, height = 30, res=600, units = "cm")
+  grid.arrange(p_stage_I_paired,p_stage_II_paired,p_stage_III_paired)
+dev.off()
+
+# FindClusters_resolution
+png(filename=paste(output_folder,"Plot_p_stage_I_unpaired.png",sep=""), width = 30, height = 30, res=600, units = "cm")
+  grid.arrange(p_stage_I_unpaired,p_stage_II_unpaired,p_stage_III_unpaired)
+dev.off()
 
 
 
 
 
-
-+ stat_compare_means(comparisons = my_comparisons, method="t.test")
 
 
 

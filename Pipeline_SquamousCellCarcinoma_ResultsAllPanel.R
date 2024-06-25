ba07468b6cb92ac90adfecfef6f43cdd0fbe1ce4
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-06-25 13:44:20 -0500

    Update Pipeline_SquamousCellCarcinoma_ResultsAllPanel.R

--- Pipeline_SquamousCellCarcinoma_ResultsAllPanel.R
+++ Pipeline_SquamousCellCarcinoma_ResultsAllPanel.R
@@ -36,3 +36,40 @@ plot_res_tumor_normal <- autoplot(pca_res_tumor_normal, data = colData[rownames(
 png(filename=paste(output_dir,"plot_res_tumor_normal.png",sep=""), width = 14, height = 14, res=600, units = "cm")
   plot_res_tumor_normal
 dev.off()
+#####################################################################################################################################################################################
+# Selection of biomarkers
+# Take genes of stage I
+# Sort by vector name [z] then [x]
+# Take genes of stage I
+# Stage specific genes
+stage_I_specific<-genes_unique_stages_stage_specific[genes_unique_stages_stage_specific[,"Stage_I.sig"]=="stage-specific",]
+stage_II_specific<-genes_unique_stages_stage_specific[genes_unique_stages_stage_specific[,"Stage_II.sig"]=="stage-specific",]
+stage_III_specific<-genes_unique_stages_stage_specific[genes_unique_stages_stage_specific[,"Stage_III.sig"]=="stage-specific",]
+
+# Filter genes by FDR < 0.01
+stage_I_specific<-stage_I_specific[stage_I_specific[,"Stage_I-normal.FDR"]<=0.01,]
+stage_II_specific<-stage_II_specific[stage_II_specific[,"Stage_II-normal.FDR"]<=0.01,]
+stage_III_specific<-stage_III_specific[stage_III_specific[,"Stage_III-normal.FDR"]<=0.01,]
+
+# Filter genes by FDR < 0.01
+stage_I_specific<-stage_I_specific[order(stage_I_specific[,"Stage_I-normal.log2fc"]),]
+stage_II_specific<-stage_II_specific[order(stage_II_specific[,"Stage_II-normal.log2fc"]),]
+stage_III_specific<-stage_III_specific[order(stage_III_specific[,"Stage_III-normal.log2fc"]),]
+#####################################################################################################################################################################################
+head(stage_I_specific,n=4)
+head(stage_II_specific,n=4)
+head(stage_III_specific,n=4)
+
+# change box plot line colors by groups
+p_stage_I_paired<-ggplot(unstranded_data_samples[unstranded_data_samples$ENSEMBL %in% head(stage_I_specific,n=4)[,c("ENSEMBL")],], aes(x=stages, y=RPKM, fill=tissue_type)) +   geom_boxplot()+ facet_wrap(~SYMBOL, ncol = 2, scales="free")+ theme_bw() + ggtitle("Stage I genes. Paired samples")
+p_stage_I_unpaired<-ggplot(unstranded_data_samples_unapaired[unstranded_data_samples_unapaired$ENSEMBL %in% head(stage_I_specific,n=4)[,c("ENSEMBL")],], aes(x=stages, y=RPKM)) +   geom_boxplot()+ facet_wrap(~SYMBOL, ncol = 2, scales="free")+ theme_bw() + ggtitle("Stage I genes. Tumor samples") #  + stat_compare_means(comparisons = my_comparisons, method="t.test") 
+
+# change box plot line colors by groups
+p_stage_II_paired<-ggplot(unstranded_data_samples[unstranded_data_samples$ENSEMBL %in% head(stage_II_specific,n=4)[,c("ENSEMBL")],], aes(x=stages, y=RPKM, fill=tissue_type)) +   geom_boxplot()+ facet_wrap(~SYMBOL, ncol = 2, scales="free")+ theme_bw() + ggtitle("Stage II genes. Paired samples")
+p_stage_II_unpaired<-ggplot(unstranded_data_samples_unapaired[unstranded_data_samples_unapaired$ENSEMBL %in% head(stage_II_specific,n=4)[,c("ENSEMBL")],], aes(x=stages, y=RPKM)) +   geom_boxplot()+ facet_wrap(~SYMBOL, ncol = 2, scales="free")+ theme_bw() + ggtitle("Stage II genes. Tumor samples") #  + stat_compare_means(comparisons = my_comparisons, method="t.test") 
+
+# change box plot line colors by groups
+p_stage_III_paired<-ggplot(unstranded_data_samples[unstranded_data_samples$ENSEMBL %in% head(stage_III_specific,n=4)[,c("ENSEMBL")],], aes(x=stages, y=RPKM, fill=tissue_type)) +   geom_boxplot()+ facet_wrap(~SYMBOL, ncol = 2, scales="free")+ theme_bw() + ggtitle("Stage III genes. Paired samples")
+p_stage_III_unpaired<-ggplot(unstranded_data_samples_unapaired[unstranded_data_samples_unapaired$ENSEMBL %in% head(stage_III_specific,n=4)[,c("ENSEMBL")],], aes(x=stages, y=RPKM)) +   geom_boxplot()+ facet_wrap(~SYMBOL, ncol = 4, scales="free")+ theme_bw() + ggtitle("Stage III genes. Tumor samples") #  + stat_compare_means(comparisons = my_comparisons, method="t.test") 
+
+

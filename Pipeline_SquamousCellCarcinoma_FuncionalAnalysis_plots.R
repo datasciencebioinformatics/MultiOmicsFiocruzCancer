025a6cc52388b1d6340378d11552bbc4e2235f23
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-07-01 14:44:03 -0500

    Update Pipeline_SquamousCellCarcinoma_FuncionalAnalysis_plots.R

--- Pipeline_SquamousCellCarcinoma_FuncionalAnalysis_plots.R
+++ Pipeline_SquamousCellCarcinoma_FuncionalAnalysis_plots.R
@@ -219,3 +219,103 @@ write.xlsx(x=matrix_count_terms_selected_KEGG,file=paste(output_dir,"unique_gene
 write.xlsx(x=matrix_count_terms_selected_Reactome,file=paste(output_dir,"unique_genes_annotation_count",".xlsx",sep=""), sheet="selected Reactome", append=TRUE)
 ####################################################################################################################
 
+
+
+
+
+
+
+
+
+####################################################################################################################
+# Concatenate table
+df_all_annotation_selected_pathways<-rbind(df_all_annotation[which(df_all_annotation$CluterProfiler %in% selection_GO),],
+interactome_annotation_stage,coexpression_annotation)
+####################################################################################################################
+# All stages
+graph_all_stages <- graph_from_data_frame(d=unique(df_all_annotation_selected_pathways[,c("Symbol","CluterProfiler")]), vertices=unique(c(df_all_annotation_selected_pathways$Symbol,df_all_annotation_selected_pathways$CluterProfiler)), directed=F)  
+
+# df_all_eges
+df_all_eges<-data.frame(get.edgelist(graph_all_stages))
+
+# Set ronames
+df_all_eges$names<-paste(df_all_eges$X1,df_all_eges$X2,sep="-")
+####################################################################################################################
+# Vertice colours of genes
+V(graph_all_stages)$color                                                                                                  <- "black"
+V(graph_all_stages)[which(names(V(graph_all_stages)) %in% df_all_annotation_selected_pathways$CluterProfiler)]$color       <- "black"
+
+stage_I   <-names(V(graph_all_stages)[which(names(V(graph_all_stages)) %in% ids_stage_I$SYMBOL)])
+stage_II  <-names(V(graph_all_stages)[which(names(V(graph_all_stages)) %in% ids_stage_II$SYMBOL)])
+stage_III <-names(V(graph_all_stages)[which(names(V(graph_all_stages)) %in% ids_stage_III$SYMBOL)])
+
+names(V(graph_all_stages)) %in% c(stage_I,stage_II,stage_III)
+length(names(V(graph_all_stages)) %in% c(stage_I,stage_II,stage_III))
+names(V(graph_all_stages))[!names(V(graph_all_stages)) %in% c(stage_I,stage_II,stage_III)]
+
+
+V(graph_all_stages)[which(names(V(graph_all_stages)) %in% ids_stage_I$SYMBOL)]$color       <- "#E69F00"
+V(graph_all_stages)[which(names(V(graph_all_stages)) %in% ids_stage_II$SYMBOL)]$color      <- "#009E73"
+V(graph_all_stages)[which(names(V(graph_all_stages)) %in% ids_stage_III$SYMBOL)]$color     <- "#D81B60"
+
+# Vertice colours of genes
+V(graph_all_stages)$shape                                                                                                                                                                                      <-"circle"
+V(graph_all_stages)[which(names(V(graph_all_stages)) %in%  df_all_annotation_selected_pathways[df_all_annotation_selected_pathways$Layer %in% c("GO","KEGG","Reactome"),"CluterProfiler"])]$shape              <- "square"
+V(graph_all_stages)[which(names(V(graph_all_stages)) %in%  df_all_annotation_selected_pathways[df_all_annotation_selected_pathways$Layer %in% c("Ontology","Disease","Pathway"),"CluterProfiler"])]$shape      <- "square"
+
+# Set size of the node according to the dregree
+V(graph_all_stages)$size                                                                                                                                                                                       <- 5
+V(graph_all_stages)[which(names(V(graph_all_stages)) %in% df_all_annotation_selected_pathways[df_all_annotation_selected_pathways$Layer %in% c("Ontology","Disease","Pathway"),"CluterProfiler"])]$size        <- 7
+V(graph_all_stages)[which(names(V(graph_all_stages)) %in% df_all_annotation_selected_pathways[df_all_annotation_selected_pathways$Layer %in% c("GO","KEGG","Reactome"),"CluterProfiler"])]$size                <- 7
+
+# Vertice colours of genes
+E(graph_all_stages)$color                                                                         <- "lightgrey"
+
+# Set ronames
+E(graph_all_stages)[which(df_all_eges$names %in% selected_interactome)]$color                     <- "black"
+E(graph_all_stages)[which(df_all_eges$names %in% selected_coexpression)]$color                    <- "darkblue"
+matrix_count_terms_selected_GO        <-df_count_terms_selected_GO[selection_GO,]
+matrix_count_terms_selected_KEGG      <-df_count_terms_selected_KEGG[selection_KEGG,]
+matrix_count_terms_selected_Reactome  <-df_count_terms_selected_Reactome[selection_Reactome,]
+
+# Set size of the node according to the dregree
+V(graph_all_stages)$label                                                                                                                                                                                      <- ""
+V(graph_all_stages)[which(names(V(graph_all_stages)) %in% df_all_annotation_selected_pathways[df_all_annotation_selected_pathways$Layer %in% c("Ontology","Disease","Pathway"),"CluterProfiler"])]$label        <- names(V(graph_all_stages)[which(names(V(graph_all_stages)) %in% df_all_annotation_selected_pathways[df_all_annotation_selected_pathways$Layer %in% c("Ontology","Disease","Pathway"),"CluterProfiler"])])
+V(graph_all_stages)[which(names(V(graph_all_stages)) %in% df_all_annotation_selected_pathways[df_all_annotation_selected_pathways$Layer %in% c("GO","KEGG","Reactome"),"CluterProfiler"])]$label                <- names(V(graph_all_stages)[which(names(V(graph_all_stages)) %in% df_all_annotation_selected_pathways[df_all_annotation_selected_pathways$Layer %in% c("GO","KEGG","Reactome"),"CluterProfiler"])])
+
+
+go_order  <-hcluster(matrix_count_terms_selected_GO[,c("Stage_I_norm","Stage_II_norm","Stage_III_norm")],link = "ave")$labels[hcluster(matrix_count_terms_selected_GO[,c("Stage_I_norm","Stage_II_norm","Stage_III_norm")],link = "ave")$order]
+kegg_order<-hcluster(matrix_count_terms_selected_KEGG[,c("Stage_I_norm","Stage_II_norm","Stage_III_norm")],link = "ave")$labels[hcluster(matrix_count_terms_selected_KEGG[,c("Stage_I_norm","Stage_II_norm","Stage_III_norm")],link = "ave")$order]
+reactome_order<-hcluster(matrix_count_terms_selected_Reactome[,c("Stage_I_norm","Stage_II_norm","Stage_III_norm")],link = "ave")$labels[hcluster(matrix_count_terms_selected_Reactome[,c("Stage_I_norm","Stage_II_norm","Stage_III_norm")],link = "ave")$order]
+
+# FindClusters_resolution
+png(filename=paste(output_folder,"Plot_Stage_all_per_Stagge.png",sep=""), width = 30, height = 30, res=600, units = "cm")
+	#plot(graph_all_stages, layout=layout_nicely, vertex.label=NA) # Stage I
+	#plot(graph_all_stages, layout=  layout_with_kk, vertex.label=NA) # Stage II
+	#plot(graph_all_stages, layout=   layout_nicely, vertex.label=V(graph_all_stages)$labels) # Stage II
+	plot(graph_all_stages, layout=   layout_nicely,vertex.label=V(graph_all_stages)$label, edge.colour=NA ) # Stage II
+dev.off()
+
+
+
+
+
+
+###########################################################################
+# FindClusters_resolution
+png(filename=paste(output_folder,"Plot_Stage_label.png",sep=""), width = 30, height = 30, res=600, units = "cm")
+	#plot(graph_all_stages, layout=layout_nicely) # Stage I
+	#plot(graph_all_stages, layout=  layout_with_kk) # Stage II
+	plot(graph_all_stages, layout=   layout_nicely) # Stage II
+dev.off()
+
+
+# Save file 
+write.xlsx(x=df_all_annotation_selected_pathways,file=paste(output_dir,"unique_genes_annotation_clusterProfiler",".xlsx",sep=""), sheet="ten most abundant per stage", append=TRUE)
+
+# Set legend
+png(filename=paste(output_folder,"legend.png",sep=""), width = 5, height = 5, res=600, units = "cm")
+plot(NULL ,xaxt='n',yaxt='n',bty='n',ylab='',xlab='', xlim=0:1, ylim=0:1)
+legend("topleft", legend =c('GO', 'KEGG', 'REACTOME','Gene'), pch=16, pt.cex=3, cex=1.5, bty='n',col = c('#ff6347', '#ffd700', '#7f7f7f', '#0072b2'))
+mtext("Legend", at=0.2, cex=2)
+###########################################################################

28d0085a0e6757ada1c4c21b3b7e5ddb30df1d37
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-07-05 15:02:58 -0500

    Update Pipeline_SquamousCellCarcinoma_ExpressionPlots.R

--- Pipeline_SquamousCellCarcinoma_ExpressionPlots.R
+++ Pipeline_SquamousCellCarcinoma_ExpressionPlots.R
@@ -207,7 +207,7 @@ my_comparisons <- list( c("Stage I", "Stage II"), c("Stage II", "Stage III"), c(
 my_vector <- annotation_stage_I$gene_i
   
 # Define the number of elements you want in each chunk 
-chunk_size <- 9
+chunk_size <- 25
   
 # Initialize an empty list to store chunks 
 chunks <- list() 
@@ -223,13 +223,8 @@ for (i in seq(1, length(my_vector), by = chunk_size)) {
 	print(chunk)
 
 	# change box plot line colors by groups
-	p_stage_I_paired<-ggplot(unstranded_data_samples[unstranded_data_samples$ENSEMBL %in% chunk,], aes(x=stages, y=RPKM, fill=tissue_type)) +   geom_boxplot()+ facet_wrap(~SYMBOL, nrow = 3,ncol = 3, scales="free")+ theme_bw() + ggtitle("Biomarkers for stage I")
-	p_stage_I_unpaired<-ggplot(unstranded_data_samples_unapaired[unstranded_data_samples_unapaired$ENSEMBL %in% chunk,], aes(x=stages, y=RPKM, fill=stages)) +   geom_boxplot()+ facet_wrap(~SYMBOL, nrow = 3,ncol = 3, scales="free")+ theme_bw() + ggtitle("Biomarkers for stage I") + stat_compare_means(comparisons = my_comparisons, vjust =0.03, label.x.npc="bottom", method="t.test") + scale_fill_manual(values=c('#e8f2a1', '#729fcf', '#ffaa95'))
-	# FindClusters_resolution
-	png(filename=paste(output_folder,paste("p_stage_I_paired_",i,".png",sep=""),sep=""), width = 24, height = 32, res=600, units = "cm")
-		print(p_stage_I_paired + theme(legend.position="bottom"))
-	dev.off()
-	png(filename=paste(output_folder,paste("p_stage_I_unpaired_",i,".png",sep=""),sep=""), width = 24, height = 32, res=600, units = "cm")
+	p_stage_I_unpaired<-ggplot(unstranded_data_samples_unapaired[unstranded_data_samples_unapaired$ENSEMBL %in% chunk,], aes(x=stages, y=RPKM, fill=stages)) +  geom_boxplot()+ facet_wrap(~SYMBOL, nrow = 5,ncol = 5, scales="free")+ theme_bw() + ggtitle("Biomarkers for stage I") + scale_fill_manual(values=c('#e8f2a1', '#729fcf', '#ffaa95')) + stat_summary(fun.y=mean, fun.min = min, fun.max = max ,geom="point", shape=20, size=8, color="red", fill="red")+ geom_jitter(aes(colour = stages), size=0.4, alpha=0.9) + scale_color_manual(values=c('#e8f2a1', '#729fcf', '#ffaa95'))	
+	png(filename=paste(output_folder,paste("p_stage_I_unpaired_",i,".png",sep=""),sep=""), width = 24, height = 24, res=600, units = "cm")
 		print(p_stage_I_unpaired + theme(legend.position="bottom"))
 	dev.off()
 }
@@ -239,13 +234,14 @@ for (i in seq(1, length(my_vector), by = chunk_size)) {
 
 
 
-c('#e8f2a1', '#729fcf', '#ffaa95')
+# Visualize: Specify the comparisons you want
+my_comparisons <- list( c("Stage I", "Stage II"), c("Stage II", "Stage III"), c("Stage I", "Stage III") )
 ############################################################################################################################################################################
 # Create sample vector 
 my_vector <- annotation_stage_II$gene_i
   
 # Define the number of elements you want in each chunk 
-chunk_size <- 9
+chunk_size <- 25
   
 # Initialize an empty list to store chunks 
 chunks <- list() 
@@ -261,19 +257,18 @@ for (i in seq(1, length(my_vector), by = chunk_size)) {
 	print(chunk)
 
 	# change box plot line colors by groups
-	p_stage_II_paired<-ggplot(unstranded_data_samples[unstranded_data_samples$ENSEMBL %in% chunk,], aes(x=stages, y=RPKM, fill=tissue_type)) +   geom_boxplot()+ facet_wrap(~SYMBOL, nrow = 3,ncol = 3, scales="free")+ theme_bw() + ggtitle("Biomarkers for stage II")
-	p_stage_II_unpaired<-ggplot(unstranded_data_samples_unapaired[unstranded_data_samples_unapaired$ENSEMBL %in% chunk,], aes(x=stages, y=RPKM, fill=stages)) +   geom_boxplot()+ facet_wrap(~SYMBOL, nrow = 3,ncol = 3, scales="free")+ theme_bw() + ggtitle("Biomarkers for stage II") + stat_compare_means(comparisons = my_comparisons, vjust =0.03, label.x.npc="bottom", method="t.test") + scale_fill_manual(values=c('#e8f2a1', '#729fcf', '#ffaa95'))
-
-	png(filename=paste(output_folder,paste("p_stage_II_unpaired_",i,".png",sep=""),sep=""), width = 24, height = 32, res=600, units = "cm")
+	p_stage_II_unpaired<-ggplot(unstranded_data_samples_unapaired[unstranded_data_samples_unapaired$ENSEMBL %in% chunk,], aes(x=stages, y=RPKM, fill=stages)) +  geom_boxplot()+ facet_wrap(~SYMBOL, nrow = 5,ncol = 5, scales="free")+ theme_bw() + ggtitle("Biomarkers for stage II") + scale_fill_manual(values=c('#e8f2a1', '#729fcf', '#ffaa95')) + stat_summary(fun.y=mean, fun.min = min, fun.max = max ,geom="point", shape=20, size=8, color="red", fill="red")+ geom_jitter(aes(colour = stages), size=0.4, alpha=0.9) + scale_color_manual(values=c('#e8f2a1', '#729fcf', '#ffaa95'))	
+	png(filename=paste(output_folder,paste("p_stage_II_unpaired_",i,".png",sep=""),sep=""), width = 24, height = 24, res=600, units = "cm")
 		print(p_stage_II_unpaired + theme(legend.position="bottom"))
-	dev.off()		
-	
-	# FindClusters_resolution
-	png(filename=paste(output_folder,paste("p_stage_II_paired_",i,".png",sep=""),sep=""), width = 24, height = 32, res=600, units = "cm")
-		print(p_stage_II_paired + theme(legend.position="bottom"))
 	dev.off()
 }
 ############################################################################################################################################################################
+
+
+
+
+
+
 
 
 
@@ -283,7 +278,7 @@ for (i in seq(1, length(my_vector), by = chunk_size)) {
 my_vector <- annotation_stage_III$gene_i
   
 # Define the number of elements you want in each chunk 
-chunk_size <- 9
+chunk_size <- 25
   
 # Initialize an empty list to store chunks 
 chunks <- list() 
@@ -299,15 +294,8 @@ for (i in seq(1, length(my_vector), by = chunk_size)) {
 	print(chunk)
 
 	# change box plot line colors by groups
-	p_stage_III_paired<-ggplot(unstranded_data_samples[unstranded_data_samples$ENSEMBL %in% chunk,], aes(x=stages, y=RPKM, fill=tissue_type)) +   geom_boxplot()+ facet_wrap(~SYMBOL, nrow = 3,ncol = 3, scales="free")+ theme_bw() + ggtitle("Biomarkers for stage III")
-	p_stage_III_unpaired<-ggplot(unstranded_data_samples_unapaired[unstranded_data_samples_unapaired$ENSEMBL %in% chunk,], aes(x=stages, y=RPKM, fill=stages)) +   geom_boxplot()+ facet_wrap(~SYMBOL, nrow = 3,ncol = 3, scales="free")+ theme_bw() + ggtitle("Biomarkers for stage III") + stat_compare_means(comparisons = my_comparisons, vjust =0.03, label.x.npc="bottom", method="t.test") + scale_fill_manual(values=c('#e8f2a1', '#729fcf', '#ffaa95'))
-	
-	# FindClusters_resolution
-	png(filename=paste(output_folder,paste("p_stage_III_paired_",i,".png",sep=""),sep=""), width = 24, height = 32, res=600, units = "cm")
-		print(p_stage_III_paired + theme(legend.position="bottom"))
-	dev.off()
-	png(filename=paste(output_folder,paste("p_stage_III_unpaired_",i,".png",sep=""),sep=""), width = 24, height = 32, res=600, units = "cm")
+	p_stage_III_unpaired<-ggplot(unstranded_data_samples_unapaired[unstranded_data_samples_unapaired$ENSEMBL %in% chunk,], aes(x=stages, y=RPKM, fill=stages)) +  geom_boxplot()+ facet_wrap(~SYMBOL, nrow = 5,ncol = 5, scales="free")+ theme_bw() + ggtitle("Biomarkers for stage II") + scale_fill_manual(values=c('#e8f2a1', '#729fcf', '#ffaa95')) + stat_summary(fun.y=mean, fun.min = min, fun.max = max ,geom="point", shape=20, size=8, color="red", fill="red")+ geom_jitter(aes(colour = stages), size=0.4, alpha=0.9) + scale_color_manual(values=c('#e8f2a1', '#729fcf', '#ffaa95'))	
+	png(filename=paste(output_folder,paste("p_stage_III_unpaired_",i,".png",sep=""),sep=""), width = 24, height = 24, res=600, units = "cm")
 		print(p_stage_III_unpaired + theme(legend.position="bottom"))
-	dev.off()	
+	dev.off()
 }
-############################################################################################################################################################################

464cf39ccc636aa70c3cab93271b1e058df8ec81
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2024-07-23 13:05:49 -0500

    Update Pipeline_SquamousCellCarcinoma_Pathview.R

--- Pipeline_SquamousCellCarcinoma_Pathview.R
+++ Pipeline_SquamousCellCarcinoma_Pathview.R
@@ -100,10 +100,28 @@ rownames(selected_genes_Stage_I_data)<-selected_genes_Stage_I_data$gene         
 rownames(selected_genes_Stage_II_data)<-selected_genes_Stage_II_data$gene                                                             #
 rownames(selected_genes_Stage_III_data)<-selected_genes_Stage_III_data$gene                                                           #
 #######################################################################################################################################
-genes<-c(rownames(selected_genes_Stage_I_data),rownames(selected_genes_Stage_II_data),rownames(selected_genes_Stage_III_data))
-logFC<-log2change_tumor_control[log2change_tumor_control$ENSEMBL %in% as.vector(genes),"log2change"]
+genes<-unique(c(rownames(selected_genes_Stage_I_data),rownames(selected_genes_Stage_II_data),rownames(selected_genes_Stage_III_data)))
+genes_ids_all<-c()
+
+# For each gene, add gene_id
+for (gene_row in genes)
+{	
+	# Store gene id in the vector
+	# Simply trim the gene id before the "." to save it in the ENSEML format
+	genes_ids_all<-c(genes_ids_all,strsplit(gene_row, split = "\\.")[[1]][1])
+}
+log2change_tumor_control$ENSEMBL<-""
+# For each gene, add gene_id
+for (gene_row in rownames(log2change_tumor_control))
+{	
+	# Store gene id in the vector
+	# Simply trim the gene id before the "." to save it in the ENSEML format
+	log2change_tumor_control[gene_row,"ENSEMBL"]<-strsplit(log2change_tumor_control[gene_row,"gene"], split = "\\.")[[1]][1]
+}
+logFC<-log2change_tumor_control[log2change_tumor_control$ENSEMBL %in% genes_ids_all,"log2change"]
 ids_stage_I      <-bitr(log2change_tumor_control$ENSEMBL, fromType = "ENSEMBL", toType = c("ENTREZID","SYMBOL"), OrgDb="org.Hs.eg.db")
-names(logFC)<-ids_stage_I[ids_stage_I$ENSEMBL %in% genes,"ENTREZID"]
+
+names(logFC)<-ids_stage_I[ids_stage_I$ENSEMBL %in% genes_ids_all,"ENTREZID"]
 
 
 pathview(gene.data=logFC*100,species="hsa",pathway="05223")
